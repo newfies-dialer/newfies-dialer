5dc90d0cbc57c1ae5c3bde7f46047be4628d1402
Areski Belaid <areski@gmail.com>
2011-06-17 16:14:46 +0200

    Class for IP authentication

--- newfies/dialer_cdr/api/urls.py
+++ newfies/dialer_cdr/api/urls.py
@@ -4,18 +4,37 @@ from piston.authentication import HttpBasicAuthentication
 from piston.doc import documentation_view
 from handlers import callrequestHandler, testcallHandler, \
 answercallHandler, hangupcallHandler, testHandler
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth import authenticate
 #from django.views.decorators.cache import cache_page
 import custom_xml_emitter
 #from common.custom_xml_emitter import CustomXmlEmitter
 
 
+class IpAuthentication(object):
+    """
+    IP Authentication handler
+    """
+    def __init__(self, auth_func=authenticate, realm='API'):
+        self.auth_func = auth_func
+        self.realm = realm
+
+    def is_authenticated(self, request):
+        return False
+
+    def challenge(self):
+        resp = HttpResponse("Not Authorized")
+        resp.status_code = 401
+        return resp
+
 auth = HttpBasicAuthentication(realm='Newfies Application')
+auth_ip = IpAuthentication()
 
 callrequest_handler = Resource(callrequestHandler, authentication=auth)
 testcall_handler = Resource(testcallHandler, authentication=auth)
 answercall_handler = Resource(answercallHandler, authentication=auth)
 hangupcall_handler = Resource(hangupcallHandler, authentication=auth)
-test_handler = Resource(testHandler, authentication=auth)
+test_handler = Resource(testHandler, authentication=auth_ip)
 
 urlpatterns = patterns('',
 

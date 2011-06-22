fa96bdf155c0e0e6b8d0c8f7e11ed42054bdf346
Areski Belaid <areski@gmail.com>
2011-06-22 12:45:51 +0200

    Add util with phonenumber_change_prefix fct

---
+++ newfies/dialer_gateway/utils.py
@@ -0,0 +1,45 @@
+from dialer_gateway.models import Gateway
+
+
+def phonenumber_change_prefix(phone_number, gateway_id):
+    """apply prefix modification for a given phone_number and gateway"""
+
+    """
+    **Attributes**:
+
+        * ``name`` - Gateway name.
+        * ``description`` - Description about Gateway.
+        * ``addprefix`` - Add prefix.
+        * ``removeprefix`` - Remove prefix.
+        * ``gateways`` - "user/,user", # Gateway string to try dialing separated by comma. First in list will be tried first
+        * ``gateway_codecs`` - "'PCMA,PCMU','PCMA,PCMU'", # Codec string as needed by FS for each gateway separated by comma
+        * ``gateway_timeouts`` - "10,10", # Seconds to timeout in string for each gateway separated by comma
+        * ``gateway_retries`` - "2,1", # Retry String for Gateways separated by comma, on how many times each gateway should be retried
+        * ``originate_dial_string`` - originate_dial_string
+        * ``secondused`` -
+        * ``failover`` -
+        * ``addparameter`` -
+        * ``count_call`` -
+        * ``count_in_use`` -
+        * ``maximum_call`` -
+        * ``status`` - Gateway status
+    """
+    try:
+        obj_gateway = Gateway.objects.get(id=gateway_id)
+    except:
+        print 'Can\'t find this Gateway : %s' % gateway_id
+        return False
+
+    if not phone_number:
+        return False
+    
+    if obj_gateway.status!=1:
+        print 'Gateway not Active: %s' % gateway_id
+        return False
+
+    if len(obj_gateway.removeprefix) > 0 and phone_number.startswith(obj_gateway.removeprefix):
+        phone_number = phone_number[len(obj_gateway.removeprefix):]
+
+    phone_number = obj_gateway.addprefix + phone_number
+
+    return phone_number

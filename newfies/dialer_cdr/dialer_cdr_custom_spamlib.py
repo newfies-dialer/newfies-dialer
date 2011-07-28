e4780da4ab5fc4859c7e833001684b9a865e4fab
Shrenik Patel <shrenik181986@gmail.com>
2011-07-28 12:11:52 +0530

    fix dilla duration & user

--- newfies/dialer_cdr/dialer_cdr_custom_spamlib.py
+++ newfies/dialer_cdr/dialer_cdr_custom_spamlib.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from dialer_cdr.models import *
 from dilla import spam
 import string
@@ -9,7 +10,13 @@ import datetime
 
 log = logging.getLogger('dilla')
 
-
 @spam.strict_handler('dialer_cdr.VoIPCall.duration')
 def get_duration(record, field):
     return random.randint(1, 100)
+
+
+@spam.strict_handler('dialer_cdr.VoIPCall.user')
+def get_user(record, field):
+    return User.objects.get(pk=1)
+
+

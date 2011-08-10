132904d9d1ca81b5482c1969c96b8725cfd50f0e
Shrenik Patel <shrenik181986@gmail.com>
2011-08-10 14:15:52 +0530

    updated doc conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -36,7 +36,7 @@ setup_environ(settings)
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.coverage']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.intersphinx', 'sphinx.ext.coverage']
 #extensions = ['sphinx.ext.autodoc', 'rst2pdf.pdfbuilder', 'sphinx.ext.coverage']
 
 # Add any paths that contain templates here, relative to this directory.

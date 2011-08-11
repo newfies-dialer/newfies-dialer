86cdc6398dddb5bcf46bff536ec91852fb375a58
Shrenik Patel <shrenik181986@gmail.com>
2011-08-11 12:13:29 +0530

    updated requirement

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,9 +23,9 @@ import sys, os
 
 APP_DIR = os.path.normpath(os.path.join(os.getcwd(), '../..')) + '/newfies/'
 sys.path.append(APP_DIR)
-import settings.sqlite
+import settings
 from django.core.management import setup_environ
-setup_environ(settings.sqlite)
+setup_environ(settings)
 
 # -- General configuration -----------------------------------------------------
 

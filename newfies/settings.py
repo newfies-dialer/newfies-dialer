91ac26b9d0132bdfe2ecc5f63f0c272082a63639
Areski Belaid <areski@gmail.com>
2011-09-23 10:28:49 +0200

    Fix I18N on Sentry

--- newfies/settings.py
+++ newfies/settings.py
@@ -132,6 +132,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     "django.core.context_processors.media",
     "django.core.context_processors.static",
     "django.contrib.messages.context_processors.messages",
+    #needed by Sentry
     "django.core.context_processors.request",
 )
 

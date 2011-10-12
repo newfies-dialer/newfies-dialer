e98e926e5f0266498c8939ae6a712e281b7d7947
Areski Belaid <areski@gmail.com>
2011-10-12 13:56:07 +0200

    Add runtests script

---
+++ runtests.sh
@@ -0,0 +1,18 @@
+
+#Run testing suit
+cd newfies
+
+#Run Full Test Suit
+./manage.py test --verbosity=2
+
+#Run NewfiesApiTestCase
+#./manage.py test dialer_cdr.NewfiesApiTestCase --verbosity=2
+
+#Run NewfiesAdminInterfaceTestCase
+#./manage.py test dialer_cdr.NewfiesAdminInterfaceTestCase --verbosity=2
+
+#Run NewfiesCustomerInterfaceTestCase
+#./manage.py test dialer_cdr.NewfiesCustomerInterfaceTestCase --verbosity=2
+
+cd -
+

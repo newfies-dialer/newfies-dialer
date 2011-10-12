749fd85051ca1aa2fc10e0d86ce3a0b73f9b1584
Shrenik Patel <shrenik181986@gmail.com>
2011-10-12 20:55:34 +0530

    updated tastypie api

--- newfies/dialer_campaign/api/resources.py
+++ newfies/dialer_campaign/api/resources.py
@@ -11,6 +11,7 @@ from tastypie.authorization import Authorization
 from tastypie.serializers import Serializer
 from tastypie.validation import Validation
 from tastypie.throttle import BaseThrottle
+from tastypie.utils import dict_strip_unicode_keys, trailing_slash
 from dialer_campaign.models import Campaign
 from tastypie import fields
 from dialer_campaign.function_def import user_attached_with_dialer_settings, check_dialer_setting
@@ -288,12 +289,37 @@ class CampaignResource(ModelResource):
         resource_name = 'campaign'
         authorization = Authorization()
         authentication = BasicAuthentication()
-        validation = CampaignValidation()
+        #validation = CampaignValidation()
         filtering = {
             'name': ALL,
             'status': ALL,
         }
 
+    def post_list(self, request, **kwargs):
+        """
+        Creates a new resource/object with the provided data.
+
+        Calls ``obj_create`` with the provided data and returns a response
+        with the new resource's location.
+
+        If a new resource is created, return ``HttpCreated`` (201 Created).
+        """
+        deserialized = self.deserialize(request, request.raw_post_data, format=request.META.get('CONTENT_TYPE', 'application/json'))
+
+        # Force this in an ugly way, at least should do "reverse"
+        temp = eval(request.raw_post_data)
+
+        #curl -u areski:areski --dump-header - -H "Content-Type: application/json" -X POST --data '{"name": "mycampaign", "description": "", "callerid": "1239876", "startingdate": "1301392136.0", "expirationdate": "1301332136.0", "frequency": "20", "callmaxduration": "50", "maxretry": "3", "intervalretry": "3000", "calltimeout": "45", "aleg_gateway": "/api/v1/gateway/1/", "voipapp": "/api/v1/voipapp/1/", "extra_data": "2000", "user": "api/v1/user/1/" }' http://localhost:8000/api/v1/campaign/
+        #error : The 'voipapp' field has was given data that was not a URI, not a dictionary-alike and does not have a 'pk' attribute: ['/api/v1/voipapp/1/']
+        deserialized["user"] = ["/api/v1/user/%s/" % request.user.id,]
+        deserialized["voipapp"] = ["%s" % temp.get('voipapp'),]
+        deserialized["aleg_gateway"] = ["%s" % temp.get('aleg_gateway'),]
+
+        bundle = self.build_bundle(data=dict_strip_unicode_keys(deserialized))
+
+        self.is_valid(bundle, request)
+        updated_bundle = self.obj_create(bundle, request=request)
+        return HttpCreated(location=self.get_resource_uri(updated_bundle))
     """
     def hydrate(self, bundle):
         startingdate = bundle.data.get('startingdate')

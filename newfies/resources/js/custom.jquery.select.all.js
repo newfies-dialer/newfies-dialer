9aa2456584f58e82272be7b948d2a3ff58b026a1
Shrenik Patel <shrenik181986@gmail.com>
2012-01-13 20:01:22 +0530

    rollback on javascript

--- newfies/resources/js/custom.jquery.select.all.js
+++ newfies/resources/js/custom.jquery.select.all.js
@@ -14,24 +14,24 @@ $(document).ready(function() {
             return false; // The form will *not* submit
         }
         else
-        {            
+        {
              var confirm_string;
              var contact_count;
              if(document.location.href.search("/voiceapp/") != -1)
              {
-                 confirm_string = ngettext('%s voice app(s) are going to be deleted?', $fields.length);
+                 confirm_string =  $fields.length + gettext(' voice app(s) are going to be deleted?');
              }
              if(document.location.href.search("/contact/") != -1)
              {
-                 confirm_string = ngettext('%s phonebook(s) are going to be deleted?', $fields.length) ;
+                 confirm_string = $fields.length +  gettext(' phonebook(s) are going to be deleted?') ;
              }
              if(document.location.href.search("/survey/") != -1)
              {
-                 confirm_string = ngettext('%s survey(s) are going to be deleted?', $fields.length);
+                 confirm_string = $fields.length + gettext(' survey(s) are going to be deleted?');
              }
              if(document.location.href.search("/audio/") != -1)
              {
-                 confirm_string = ngettext('%s audio(s) are going to be deleted?', $fields.length);
+                 confirm_string = $fields.length + gettext(' audio(s) are going to be deleted?');
              }
              if(document.location.href.search("/phonebook/") != -1)
              {
@@ -54,20 +54,20 @@ $(document).ready(function() {
                     }
                 });
                 
-                 confirm_string = ngettext('%s phonebook(s) are going to be deleted with %s contact(s)?', $fields.length, contact_count);
+                 confirm_string = $fields.length + gettext(' phonebook(s) are going to be deleted with') + contact_count + gettext(' contact(s)?');
              }             
              if(document.location.href.search("/campaign/") != -1)
              {
-                 confirm_string = ngettext('%s campaign(s) are going to be deleted?', $fields.length );
+                 confirm_string = $fields.length + gettext(' campaign(s) are going to be deleted?');
              }
 
              if(document.location.href.search("/user_detail_change/") != -1)
              {
-                 confirm_string = ngettext('%s notification(s) are going to be deleted?', $fields.length);
+                 confirm_string = $fields.length + gettext(' notification(s) are going to be deleted?');
                  
                  if(document.getElementById('id_mark_read').value == 'true')
                  {
-                   confirm_string = ngettext('%s notification(s) are going to be marked as read?', $fields.length);
+                   confirm_string = $fields.length + gettext(' notification(s) are going to be marked as read?');
                  }
              }
 

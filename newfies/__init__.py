7503367828da4f3fa1d6e5035261cf05773b6949
Areski Belaid <areski@gmail.com>
2012-01-14 20:07:29 +0100

    update version

--- newfies/__init__.py
+++ newfies/__init__.py
@@ -3,7 +3,7 @@
 # :copyright: (c) 2010 - 2011 by Arezqui Belaid.
 # :license:   AGPL, see COPYING for more details.
 
-VERSION = (1, 1, 0, "a")
+VERSION = (1, 1, 0, "b1")
 __version__ = ".".join(map(str, VERSION[0:3])) + "".join(VERSION[3:])
 __author__ = "Arezqui Belaid"
 __contact__ = "info@star2billing.com"

288e6b7ae9d694ff75b6a24c5a25616fbc2d40d1
Areski Belaid <areski@gmail.com>
2012-01-16 16:53:18 +0100

    Force the convertion to wav format mono 8000Hz

--- newfies/settings.py
+++ newfies/settings.py
@@ -443,12 +443,12 @@ LOGGING = {
 }
 
 # Frontend widget values
-CHANNEL_TYPE_VALUE = 0  # 0-Keep original, 1-Mono, 2-Stereo
+CHANNEL_TYPE_VALUE = 1  # 0-Keep original, 1-Mono, 2-Stereo
 
 FREQ_TYPE_VALUE = 8000  # 0-Keep original, 8000-8000Hz, 16000-16000Hz, 22050-22050Hz,
                      # 44100-44100Hz, 48000-48000Hz, 96000-96000Hz
 
-CONVERT_TYPE_VALUE = 0 # 0-Keep original, 1-Convert to MP3, 2-Convert to WAV, 3-Convert to OGG
+CONVERT_TYPE_VALUE = 2 # 0-Keep original, 1-Convert to MP3, 2-Convert to WAV, 3-Convert to OGG
 
 AUDIO_DEBUG = False
 

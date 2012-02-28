41d72ba1734c5cc4413d4b427a969f7b48c103a1
Areski Belaid <areski@gmail.com>
2012-02-28 13:36:24 +0100

    update test script

--- runtests.sh
+++ runtests.sh
@@ -3,19 +3,35 @@
 cd newfies
 
 #Run Full Test Suit
-./manage.py test --verbosity=2
+#./manage.py test --verbosity=2
 
 #Run NewfiesApiTestCase
-#./manage.py test dialer_cdr.NewfiesApiTestCase --verbosity=2
+echo "manage.py test dialer_cdr.NewfiesApiTestCase --verbosity=2"
+./manage.py test dialer_cdr.NewfiesApiTestCase --verbosity=2
+echo ""
+echo "Press any key to continue..."
+read TEMP
 
 #Run NewfiesAdminInterfaceTestCase
-#./manage.py test dialer_cdr.NewfiesAdminInterfaceTestCase --verbosity=2
+echo "manage.py test dialer_cdr.NewfiesAdminInterfaceTestCase --verbosity=2"
+./manage.py test dialer_cdr.NewfiesAdminInterfaceTestCase --verbosity=2
+echo ""
+echo "Press any key to continue..."
+read TEMP
 
 #Run NewfiesCustomerInterfaceTestCase
-#./manage.py test dialer_cdr.NewfiesCustomerInterfaceTestCase --verbosity=2
+echo "manage.py test dialer_cdr.NewfiesCustomerInterfaceTestCase --verbosity=2"
+./manage.py test dialer_cdr.NewfiesCustomerInterfaceTestCase --verbosity=2
+echo ""
+echo "Press any key to continue..."
+read TEMP
 
 #Run NewfiesTastypieApiTestCase
-#python manage.py test dialer_cdr.NewfiesTastypieApiTestCase.test_create_campaign --verbosity=2
+echo "manage.py test dialer_cdr.NewfiesTastypieApiTestCase.test_create_campaign --verbosity=2"
+./manage.py test dialer_cdr.NewfiesTastypieApiTestCase.test_create_campaign --verbosity=2
+echo ""
+echo "Press any key to continue..."
+read TEMP
 
 cd -
 

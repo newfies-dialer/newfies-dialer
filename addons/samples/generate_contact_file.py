390bed31932c62e63ccd96b07855000986a7176e
Areski Belaid <areski@gmail.com>
2012-03-28 19:26:13 +0200

    update contact script + gitignore

--- addons/samples/generate_contact_file.py
+++ addons/samples/generate_contact_file.py
@@ -8,12 +8,12 @@ import namegen
 
 test = namegen.NameGenerator()
 count_contact = 0
-max_contact = 100
+max_contact = 100000
 start_phonenumber = 640200000
 
 for it_name in test:
     count_contact = count_contact + 1
-    print "%d,	%s,	%s,	%s@newmailer.com,	test subscriber,	1," % (start_phonenumber, it_name, it_name, it_name)
+    print "%d,	%s%d,	%s%d,	%s%d@newmailer.com,	test subscriber,	1," % (start_phonenumber, it_name, count_contact, it_name, count_contact, it_name, count_contact)
     start_phonenumber = start_phonenumber + 1
     
     if count_contact > max_contact:

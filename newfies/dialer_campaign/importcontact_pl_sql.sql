fc25fc9327d1c50b09ee06db1b5728f9671bd382
Shrenik Patel <shrenik181986@gmail.com>
2012-03-28 16:56:50 +0530

    updated sql procedure

--- newfies/dialer_campaign/importcontact_pl_sql.sql
+++ newfies/dialer_campaign/importcontact_pl_sql.sql
@@ -4,19 +4,17 @@ BEGIN
 
     DECLARE campaign_id INT;
     DECLARE phonebook_id INT;
-    DECLARE cur CURSOR FOR SELECT id, contact FROM dialer_contact WHERE phonebook_id=phonebook_id;
+    DECLARE cur CURSOR FOR SELECT id, contact FROM dialer_contact WHERE phonebook_id=1;
 
     OPEN cur;
-
     read_loop: LOOP
-        FETCH cur INTO id, contact;
+        FETCH cur INTO campaign_id;
 
         INSERT INTO dialer_campaign_subscriber (contact_id, campaign_id, duplicate_contact, status, created_date, updated_date) VALUES (id, campaign_id, contact, 1, NOW(), NOW());
 
     END LOOP;
-
     CLOSE cur;
 
 END;
 //
-delimiter ;
+delimiter;

855bda6f6fc27fdf286d05bb670eb1c05e61b60d
Shrenik Patel <shrenik181986@gmail.com>
2012-03-28 17:47:46 +0530

    added arguments in sql procedure

--- newfies/dialer_campaign/importcontact_pl_sql.sql
+++ newfies/dialer_campaign/importcontact_pl_sql.sql
@@ -4,19 +4,17 @@ DROP PROCEDURE IF EXISTS importcontact_pl_sql;
 
 DELIMITER //
 
-CREATE PROCEDURE `importcontact_pl_sql` ()
+CREATE PROCEDURE `importcontact_pl_sql` (IN campaign_id INT(10), IN phonebook_id INT(10))
 LANGUAGE SQL  
 BEGIN
 
-    DECLARE campaign_id INT;
-    DECLARE phonebook_id INT;
-    DECLARE cur CURSOR FOR SELECT id, contact FROM dialer_contact WHERE phonebook_id=phonebook_id;
+    DECLARE cur CURSOR FOR SELECT `id`, `contact` FROM `dialer_contact` WHERE phonebook_id=phonebook_id;
 
     OPEN cur;
     read_loop: LOOP
         FETCH cur INTO campaign_id;
 
-        INSERT INTO dialer_campaign_subscriber (contact_id, campaign_id, duplicate_contact, status, created_date, updated_date) VALUES (id, campaign_id, contact, 1, NOW(), NOW());
+        INSERT INTO `dialer_campaign_subscriber` (`contact_id`, `campaign_id`, `duplicate_contact`, `status`, `created_date`, `updated_date`) VALUES (id, campaign_id, contact, 1, NOW(), NOW());
 
     END LOOP;
     CLOSE cur;

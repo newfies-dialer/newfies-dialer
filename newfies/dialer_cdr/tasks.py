53da6efd1090aca0f05dfd8c50d9431326340973
Areski Belaid <areski@gmail.com>
2012-03-30 19:27:04 +0200

    Fix heartbeat on tasks

--- newfies/dialer_cdr/tasks.py
+++ newfies/dialer_cdr/tasks.py
@@ -16,7 +16,7 @@ from celery.task import Task, PeriodicTask
 from dialer_campaign.models import Campaign, CampaignSubscriber
 from dialer_campaign.function_def import user_dialer_setting
 from dialer_cdr.models import Callrequest, VoIPCall
-from celery.decorators import task
+from celery.decorators import task, periodic_task
 from datetime import datetime, timedelta
 from time import sleep
 from uuid import uuid1
@@ -29,48 +29,47 @@ from django.utils.hashcompat import md5_constructor as md5
 LOCK_EXPIRE = 60 * 1 # Lock expires in 1 minute
 
 
-class callrequest_pending(PeriodicTask):
-    """A periodic task that checks for pending calls
+def single_instance_task(timeout):
+    def task_exc(func):
+        def wrapper(*args, **kwargs):
+            lock_id = "celery-single-instance-" + func.__name__
+            print lock_id
+            acquire_lock = lambda: cache.add(lock_id, "true", timeout)
+            release_lock = lambda: cache.delete(lock_id)
+            if acquire_lock():
+                try:
+                    func(*args, **kwargs)
+                finally:
+                    release_lock()
+        return wrapper
+    return task_exc
 
-    **Usage**:
+"""
+@periodic_task(run_every=timedelta(seconds=1))
+@single_instance_task(LOCK_EXPIRE)
+def callrequest_pending(*args, **kwargs):
+    #A periodic task that checks for pending calls 
+    #**Usage**:
+    #    callrequest_pending.delay()
+    #
+    logger = callrequest_pending.get_logger()
+    logger.info("TASK :: callrequest_pending")
 
-        callrequest_pending.delay()
-    """
-    run_every = timedelta(seconds=1) # every seconds
+    #TODO: Django 1.4 select_for_update
+    list_callrequest = Callrequest.objects.get_pending_callrequest()[:settings.MAX_CALLS_PER_SECOND]
+    logger.info("callrequest_pending - number_found=%d" % len(list_callrequest))
 
-    def run(self, **kwargs):
-        logger = self.get_logger(**kwargs)
-        logger.info("TASK :: callrequest_pending")
+    if not list_callrequest:
+        logger.debug("No Pending Calls")
 
-        lock_id = "%s-lock" % (self.name)
-        
-        # cache.add fails if if the key already exists
-        acquire_lock = lambda: cache.add(lock_id, "true", LOCK_EXPIRE)
-        # memcache delete is very slow, but we have to use it to take
-        # advantage of using add() for atomic locking
-        release_lock = lambda: cache.delete(lock_id)
+    for callrequest in list_callrequest:
+        logger.info("=> CallRequest (id:%s, phone_number:%s)" %
+                    (callrequest.id, callrequest.phone_number))
 
-        if acquire_lock():
-
-            #TODO: Django 1.4 select_for_update
-            list_callrequest = Callrequest.objects.get_pending_callrequest()[:settings.MAX_CALLS_PER_SECOND]
-            logger.info("callrequest_pending - number_found=%d" % len(list_callrequest))
-
-            if not list_callrequest:
-                logger.debug("No Pending Calls")
-
-            for callrequest in list_callrequest:
-                logger.info("=> CallRequest (id:%s, phone_number:%s)" %
-                            (callrequest.id, callrequest.phone_number))
-
-                callrequest.status = 7 # Update to Process
-                callrequest.save()
-                init_callrequest.delay(callrequest.id, callrequest.campaign.id)
-                
-            #Release lock    
-            release_lock()
-        else:
-            logger.error("ERROR :: %s is already being imported by another worker" % (self.name))
+        callrequest.status = 7 # Update to Process
+        callrequest.save()
+        init_callrequest.delay(callrequest.id, callrequest.campaign.id)
+"""
 
 
 @task()
@@ -83,6 +82,8 @@ def init_callrequest(callrequest_id, campaign_id):
     """
     logger = init_callrequest.get_logger()
     obj_callrequest = Callrequest.objects.get(id=callrequest_id)
+    obj_callrequest.status = 7 # Update to Process
+    obj_callrequest.save()
     logger.info("TASK :: init_callrequest - status = %s" %
                                         str(obj_callrequest.status))
 

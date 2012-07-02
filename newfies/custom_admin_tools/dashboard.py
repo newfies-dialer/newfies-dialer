7fdb6307a18947bc77883a63f22c016b3762142b
Areski Belaid <areski@gmail.com>
2012-07-02 15:57:08 +0200

    clean

--- newfies/custom_admin_tools/dashboard.py
+++ newfies/custom_admin_tools/dashboard.py
@@ -2,12 +2,12 @@
 # Newfies-Dialer License
 # http://www.newfies-dialer.org
 #
-# This Source Code Form is subject to the terms of the Mozilla Public 
+# This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this file,
 # You can obtain one at http://mozilla.org/MPL/2.0/.
 #
 # Copyright (C) 2011-2012 Star2Billing S.L.
-# 
+#
 # The Initial Developer of the Original Code is
 # Arezqui Belaid <info@star2billing.com>
 #
@@ -27,6 +27,7 @@ from admin_tools.dashboard import modules, Dashboard, AppIndexDashboard
 from admin_tools.utils import get_admin_site_name
 from admin_tools_stats.modules import DashboardCharts, get_active_graph
 from django.conf import settings
+
 
 class HistoryDashboardModule(modules.LinkList):
     title = 'History'
@@ -57,13 +58,6 @@ class CustomIndexDashboard(Dashboard):
         # we want a 3 columns layout
         self.columns = 3
 
-        site_name = get_admin_site_name(context)
-
-        #self.children.append(
-        #            HistoryDashboardModule()
-        #)
-        
-        
         self.children.append(modules.Group(
             title="General",
             display="tabs",
@@ -83,7 +77,7 @@ class CustomIndexDashboard(Dashboard):
                 modules.RecentActions(_('Recent Actions'), 5),
             ]
         ))
-        
+
         self.children.append(modules.AppList(
             _('Settings'),
             models=('dialer_settings.*', ),
@@ -100,7 +94,7 @@ class CustomIndexDashboard(Dashboard):
             _('Voip Server'),
             models=('voice_app.*', ),
         ))
-        
+
         # append an app list module for "Dialer"
         self.children.append(modules.AppList(
             _('Audio Files'),
@@ -115,6 +109,8 @@ class CustomIndexDashboard(Dashboard):
 
         # append a link list module for "quick links"
         """
+        site_name = get_admin_site_name(context)
+
         #Quick link seems to broke the admin design if too many element
         self.children.append(modules.LinkList(
             _('Quick links'),
@@ -130,7 +126,7 @@ class CustomIndexDashboard(Dashboard):
             ],
         ))
         """
-        
+
         if not settings.DEBUG:
             # append a feed module
             self.children.append(modules.Feed(
@@ -138,7 +134,7 @@ class CustomIndexDashboard(Dashboard):
                 feed_url='http://www.newfies-dialer.org/category/blog/feed/',
                 limit=5
             ))
-        
+
         # append an app list module for "Country_prefix"
         self.children.append(modules.AppList(
             _('Dashboard Stats Settings'),
@@ -151,12 +147,10 @@ class CustomIndexDashboard(Dashboard):
             kwargs = {}
             kwargs['chart_size'] = "360x100"
             kwargs['graph_key'] = i.graph_key
-            if request.POST.get('select_box_'+i.graph_key):
-                kwargs['select_box_'+i.graph_key] = request.POST['select_box_'+i.graph_key]
+            if request.POST.get('select_box_' + i.graph_key):
+                kwargs['select_box_' + i.graph_key] = request.POST['select_box_' + i.graph_key]
 
             self.children.append(DashboardCharts(**kwargs))
-
-        
 
 
 class CustomAppIndexDashboard(AppIndexDashboard):

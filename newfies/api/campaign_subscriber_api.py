b4a4d91928b0f7be8a6db38bb527edefa0f58b99
Shrenik Patel <shrenik181986@gmail.com>
2012-07-04 12:38:13 +0530

    fixed error on campaignsubscriber api

--- newfies/api/campaign_subscriber_api.py
+++ newfies/api/campaign_subscriber_api.py
@@ -223,9 +223,15 @@ class CampaignSubscriberResource(ModelResource):
         temp_url = request.META['PATH_INFO']
         temp_id = temp_url.split('/api/v1/campaignsubscriber/')[1]
         campaign_id = temp_id.split('/')[0]
-    
+
         try:
             campaign_obj = Campaign.objects.get(id=campaign_id)
+        except:
+            error_msg = "Campaign ID does not exists!"
+            logger.error(error_msg)
+            raise BadRequest(error_msg)
+
+        try:
             campaignsubscriber = CampaignSubscriber.objects\
             .get(duplicate_contact=bundle.data.get('contact'),
                 campaign=campaign_obj)

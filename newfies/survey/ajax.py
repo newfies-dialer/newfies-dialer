95f4524e74b2ccb51a8266b4b7b7b455c9ec65f7
Areski Belaid <areski@gmail.com>
2012-07-05 22:11:24 +0200

    clean code

--- newfies/survey/ajax.py
+++ newfies/survey/ajax.py
@@ -2,38 +2,39 @@
 # Newfies-Dialer License
 # http://www.newfies-dialer.org
 #
-# This Source Code Form is subject to the terms of the Mozilla Public 
+# This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this file,
 # You can obtain one at http://mozilla.org/MPL/2.0/.
 #
 # Copyright (C) 2011-2012 Star2Billing S.L.
-# 
+#
 # The Initial Developer of the Original Code is
 # Arezqui Belaid <info@star2billing.com>
 #
 
 from django.contrib.auth.models import User
-from django.utils import simplejson
-from django.utils import simplejson
 from django.contrib.auth.decorators import login_required
 
 from dajaxice.decorators import dajaxice_register
 from dajax.core import Dajax
 
 from survey.models import *
-from survey.forms import SurveyQuestionForm, SurveyQuestionNewForm, SurveyResponseForm
+from survey.forms import SurveyQuestionForm, \
+                        SurveyQuestionNewForm, \
+                        SurveyResponseForm
 
 
 def audio_msg_type(audio_message):
     if audio_message is None:
-        return 2 # Text2Speech
+        return 2  # Text2Speech
     else:
-        return 1 # Audio File
+        return 1  # Audio File
 
 
 @login_required
 @dajaxice_register
-def survey_question_add_update(request, id, data, form_type, after_initial_save, record_id):
+def survey_question_add_update(request, id, data, form_type,
+    after_initial_save, record_id):
     """ Ajax method to update the question """
     dajax = Dajax()
 
@@ -41,8 +42,8 @@ def survey_question_add_update(request, id, data, form_type, after_initial_save,
         form = SurveyQuestionForm(data)
 
         if after_initial_save == 1 and record_id:
-            survey_question = SurveyQuestion.objects.get(pk=int(record_id))            
-        else:            
+            survey_question = SurveyQuestion.objects.get(pk=int(record_id))
+        else:
             survey_question = SurveyQuestion.objects.get(pk=int(id))
 
         form = SurveyQuestionForm(data, instance=survey_question)
@@ -56,8 +57,8 @@ def survey_question_add_update(request, id, data, form_type, after_initial_save,
             audio_message = form.cleaned_data.get('audio_message')
             object.message_type = audio_msg_type(audio_message)
             object.save()
-            
-        if form_type == 'new_form':            
+
+        if form_type == 'new_form':
             obj = form.save(commit=False)
             obj.user = User.objects.get(username=request.user)
             obj.surveyapp = form.cleaned_data.get('surveyapp')
@@ -66,22 +67,25 @@ def survey_question_add_update(request, id, data, form_type, after_initial_save,
             obj.type = form.cleaned_data.get('type')
             obj.save()
             dajax.assign('#id', 'value', obj.id)
-        #dajax.alert("%s is successfully saved !!" % form.cleaned_data.get('question'))
+        #dajax.alert("%s is successfully saved !!" % \
+        #    form.cleaned_data.get('question'))
     else:
         if form_type == 'new_form':
-            dajax.remove_css_class('#new_survey_que_form_'+id+' input','error')
+            dajax.remove_css_class('#new_survey_que_form_' + id + ' input', \
+                                    'error')
         if form_type == 'old_form':
-            dajax.remove_css_class('#que_form_'+id+' input','error')
+            dajax.remove_css_class('#que_form_' + id + ' input', 'error')
         dajax.alert("error")
     return dajax.json()
 
 
 @login_required
 @dajaxice_register
-def survey_response_add_update(request, id, que_id, data, form_type, new_response_id):
+def survey_response_add_update(request, id, que_id, data,
+    form_type, new_response_id):
     """ Ajax method to update the response """
     dajax = Dajax()
-    
+
     # 1st check survey question id
     if int(que_id) != 0:
         try:
@@ -103,36 +107,43 @@ def survey_response_add_update(request, id, que_id, data, form_type, new_respons
         form = SurveyResponseForm(data)
 
         if int(new_response_id) != 0:
-            survey_response = SurveyResponse.objects.get(pk=int(new_response_id))
+            survey_response = SurveyResponse.objects\
+                                    .get(pk=int(new_response_id))
             form = SurveyResponseForm(data, instance=survey_response)
 
     if form.is_valid():
         key = form.cleaned_data.get("key")
         keyvalue = form.cleaned_data.get('keyvalue')
+        #TODO: Check keyvalue, this vars is never used
 
         duplicate_count = 0
         # start checking of duplicate key
         if form_type == 'old_form':
             resp_obj = SurveyResponse.objects.get(pk=id)
             if resp_obj.key != key:
-                duplicate_count = SurveyResponse.objects.filter(key=key,
-                                                    surveyquestion=resp_obj.surveyquestion).count()
+                duplicate_count = SurveyResponse.objects\
+                                    .filter(key=key,
+                                        surveyquestion=resp_obj.surveyquestion)\
+                                    .count()
 
         if form_type == 'new_form':
-            if que_id == 0: # without que id
+            if que_id == 0:  # without que id
                 # get last inserted record
-                surveyquestion = SurveyQuestion.objects.all().order_by('-id')[0]
-            else: # with que id
+                surveyquestion = SurveyQuestion.objects\
+                                        .all().order_by('-id')[0]
+            else:  # with que id
                 surveyquestion = SurveyQuestion.objects.get(pk=que_id)
             duplicate_count = SurveyResponse.objects.filter(key=key,
-                                                    surveyquestion=surveyquestion).count()
+                                    surveyquestion=surveyquestion).count()
 
         if duplicate_count >= 1 and form_type == 'old_form':
             #dajax.alert("error : (%s) duplicate record key & previous key (%s) is not changed!" % (key,
             #                                                                                       resp_obj.key))
             return dajax.json()
 
-        if duplicate_count >= 1 and form_type == 'new_form' and int(new_response_id) == 0:
+        if duplicate_count >= 1 \
+            and form_type == 'new_form' \
+            and int(new_response_id) == 0:
             #dajax.alert("error : (%s) duplicate record key !" % (key))
             return dajax.json()
 
@@ -146,16 +157,18 @@ def survey_response_add_update(request, id, que_id, data, form_type, new_respons
             obj.save()
             dajax.assign('#new_response_id', 'value', obj.id)
 
-        #dajax.alert("(%s - %s) is successfully saved !!" % (form.cleaned_data.get('key'),
-        #                                                   form.cleaned_data.get('keyvalue')))
+        # dajax.alert("(%s - %s) is successfully saved !!" % \
+        #        (form.cleaned_data.get('key'),
+        #        form.cleaned_data.get('keyvalue')))
     else:
         if form_type == 'new_form':
-            dajax.remove_css_class('#new_survey_que_form_'+id+' input','error')
+            dajax.remove_css_class('#new_survey_que_form_' + id + ' input', \
+                                    'error')
         if form_type == 'old_form':
-            dajax.remove_css_class('#que_form_'+id+' input','error')
+            dajax.remove_css_class('#que_form_' + id + ' input', 'error')
         dajax.alert("error")
         for error in form.errors:
-            dajax.add_css_class('#id_%s' % error,'error')
+            dajax.add_css_class('#id_%s' % error, 'error')
 
     return dajax.json()
 
@@ -167,16 +180,17 @@ def survey_question_delete(request, id):
     dajax = Dajax()
     try:
         survey_question = SurveyQuestion.objects.get(pk=int(id))
-        que = survey_question.question
-        survey_response_list = SurveyResponse.objects.filter(surveyquestion=survey_question)
+        survey_response_list = SurveyResponse.objects\
+                        .filter(surveyquestion=survey_question)
         for survey_resp in survey_response_list:
             survey_resp.delete()
         survey_question.delete()
-        #dajax.alert("(%s) is successfully deleted !!" % (que))
+        # dajax.alert("(%s) is successfully deleted !!" % \
+        #    (survey_question.question))
     except:
         dajax.alert("%s is not exist !!" % (id))
         for error in form.errors:
-            dajax.add_css_class('#id_%s' % error,'error')
+            dajax.add_css_class('#id_%s' % error, 'error')
     return dajax.json()
 
 
@@ -187,14 +201,14 @@ def survey_response_delete(request, id):
     dajax = Dajax()
     try:
         survey_response = SurveyResponse.objects.get(pk=int(id))
-        key = survey_response.key
-        keyvalue = survey_response.keyvalue
         survey_response.delete()
-        #dajax.alert("(%s - %s) is successfully deleted !!" % (key, keyvalue))
+        # key = survey_response.key
+        # keyvalue = survey_response.keyvalue
+        # dajax.alert("(%s - %s) is successfully deleted !!" % (key, keyvalue))
     except:
         dajax.alert("%s is not exist !!" % (id))
         for error in form.errors:
-            dajax.add_css_class('#id_%s' % error,'error')
+            dajax.add_css_class('#id_%s' % error, 'error')
     return dajax.json()
 
 
@@ -202,14 +216,15 @@ def survey_response_delete(request, id):
 @dajaxice_register
 def survey_question_sort(request, id, sort_order):
     dajax = Dajax()
-    
+
     try:
         survey_question = SurveyQuestion.objects.get(pk=int(id))
         survey_question.order = sort_order
         survey_question.save()
-        #dajax.alert("(%s) has been successfully sorted !!" % (survey_question.question))
+        # dajax.alert("(%s) has been successfully sorted !!" % \
+        #    (survey_question.question))
     except:
         #dajax.alert("%s is not exist !!" % (id))
         for error in form.errors:
-            dajax.add_css_class('#id_%s' % error,'error')
+            dajax.add_css_class('#id_%s' % error, 'error')
     return dajax.json()

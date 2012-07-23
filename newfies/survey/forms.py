ce0c2bd202e9ca9a48d5015c9d3fc3684952cda4
Areski Belaid <areski@gmail.com>
2012-07-23 13:31:03 +0200

    remove data and gateway on survey question edition

--- newfies/survey/forms.py
+++ newfies/survey/forms.py
@@ -69,8 +69,8 @@ class SurveyQuestionForm(ModelForm):
 
     class Meta:
         model = SurveyQuestion
-        fields = ['question', 'surveyapp', 'audio_message', 'type',
-                  'data', 'gateway']
+        fields = ['question', 'surveyapp', 'audio_message', 'type']
+        # remove those fields for now 'data', 'gateway'
 
     def __init__(self, user, *args, **kwargs):
         super(SurveyQuestionForm, self).__init__(*args, **kwargs)
@@ -80,7 +80,7 @@ class SurveyQuestionForm(ModelForm):
         self.fields['audio_message'].widget.attrs['class'] = 'span2'
         self.fields['type'].choices = APP_TYPE
         self.fields['type'].widget.attrs['class'] = 'span2'
-        self.fields['gateway'].widget.attrs['class'] = 'span2'
+        #self.fields['gateway'].widget.attrs['class'] = 'span2'
 
 
 class SurveyResponseForm(ModelForm):

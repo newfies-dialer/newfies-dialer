7038328059078403ad5ea2fd04d5d94dd414b52e
Shrenik Patel <shrenik181986@gmail.com>
2012-07-24 14:24:54 +0530

    removed template tag

--- newfies/dialer_cdr/templatetags/dialer_cdr_extras.py
+++ newfies/dialer_cdr/templatetags/dialer_cdr_extras.py
@@ -174,35 +174,6 @@ def listsort(value):
         listsort.is_safe = True
 
 
-def get_fieldset(parser, token):
-    try:
-        name, fields, as_, variable_name, from_, form = token.split_contents()
-    except ValueError:
-        raise template.TemplateSyntaxError('bad arguments for %r' %\
-                        token.split_contents()[0])
-
-    return FieldSetNode(fields.split(','), variable_name, form)
-
-
-class FieldSetNode(template.Node):
-    def __init__(self, fields, variable_name, form_variable):
-        self.fields = fields
-        self.variable_name = variable_name
-        self.form_variable = form_variable
-
-    def render(self, context):
-
-        form = template.Variable(self.form_variable).resolve(context)
-        new_form = copy.copy(form)
-        new_form.fields = \
-        SortedDict([(key, value) for key, value in form.fields.items()\
-                                 if key in self.fields])
-
-        context[self.variable_name] = new_form
-
-        return u''
-
-
 class ArgumentError(ValueError):
     """Missing or incompatible argument."""
 
@@ -319,5 +290,3 @@ register.filter('leg_type_name', leg_type_name)
 register.filter('que_res_string', que_res_string)
 register.filter('action_type_name', action_type_name)
 
-
-get_fieldset = register.tag(get_fieldset)

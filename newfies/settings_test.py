fcd4a47a28ea43ac0d44d3d43e26a285657e7413
Areski Belaid <areski@gmail.com>
2012-07-30 19:16:43 +0200

    more on test

---
+++ newfies/settings_test.py
@@ -0,0 +1,26 @@
+#
+# Newfies-Dialer License
+# http://www.newfies-dialer.org
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+# Copyright (C) 2011-2012 Star2Billing S.L.
+#
+# The Initial Developer of the Original Code is
+# Arezqui Belaid <info@star2billing.com>
+#
+
+
+#IMPORT SETTINGS
+#===============
+from settings import *
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': ':memory:',
+    }
+}

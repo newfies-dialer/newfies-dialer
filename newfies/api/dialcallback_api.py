b10f179aa8fb27a767ce0a5b87ef4d34f34cba18
Areski Belaid <areski@gmail.com>
2012-07-30 17:01:59 +0200

    fix depreciate override_url

--- newfies/api/dialcallback_api.py
+++ newfies/api/dialcallback_api.py
@@ -99,8 +99,8 @@ class DialCallbackResource(ModelResource):
         throttle = BaseThrottle(throttle_at=1000,
             timeframe=3600)
 
-    def override_urls(self):
-        """Override url"""
+    def prepend_urls(self):
+        """Prepend urls"""
         return [
             url(r'^(?P<resource_name>%s)/$' %\
                 self._meta.resource_name, self.wrap_view('create')),

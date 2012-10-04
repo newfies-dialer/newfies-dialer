0314839429c4f536fa0f1067d4e2abcc504d5755
Areski Belaid <areski@gmail.com>
2012-10-05 01:15:43 +0200

    add gunicorn in requirement + install supervisor

--- install/install-newfies.sh
+++ install/install-newfies.sh
@@ -264,7 +264,7 @@ func_install_frontend(){
     case $DIST in
         'DEBIAN')
             apt-get -y install python-setuptools python-dev build-essential
-            apt-get -y install libapache2-mod-python libapache2-mod-wsgi
+            apt-get -y install nginx supervisor
             apt-get -y install git-core mercurial gawk
             easy_install pip
 

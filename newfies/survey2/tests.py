fce46e567e9960151ecab8aff33e5b8ebf52f172
Shrenik Patel <shrenik181986@gmail.com>
2012-10-05 17:51:17 +0530

    updated testcases

--- newfies/survey2/tests.py
+++ newfies/survey2/tests.py
@@ -15,7 +15,8 @@
 from django.contrib.auth.models import User
 from django.test import TestCase
 from common.utils import BaseAuthenticatedClient
-from survey2.models import Survey, Section, Branching, Result, \
+from survey2.models import Survey, Survey_template, Section,\
+    Section_template, Branching, Branching_template, Result, \
     ResultAggregate
 from survey2.forms import SurveyForm, VoiceSectionForm,\
     MultipleChoiceSectionForm, RatingSectionForm,\
@@ -26,7 +27,7 @@ from survey2.views import survey_list, survey_grid, survey_add, \
     survey_change, survey_del, section_add, section_change,\
     section_phrasing_change, section_branch_change, survey_report,\
     survey_finestatemachine, export_surveycall_report, section_branch_add,\
-    section_delete, section_phrasing_play
+    section_delete, section_phrasing_play, survey_view
 from survey2.ajax import section_sort
 from utils.helper import grid_test_data
 
@@ -38,32 +39,32 @@ class SurveyAdminView(BaseAuthenticatedClient):
 
     def test_admin_survey_view_list(self):
         """Test Function to check admin surveyapp list"""
-        response = self.client.get('/admin/survey2/survey/')
+        response = self.client.get('/admin/survey2/survey_template/')
         self.failUnlessEqual(response.status_code, 200)
 
     def test_admin_survey_view_add(self):
         """Test Function to check admin surveyapp add"""
-        response = self.client.get('/admin/survey2/survey/add/')
+        response = self.client.get('/admin/survey2/survey_template/add/')
         self.failUnlessEqual(response.status_code, 200)
 
     def test_admin_survey_section_view_list(self):
         """Test Function to check admin surveyquestion list"""
-        response = self.client.get('/admin/survey2/section/')
+        response = self.client.get('/admin/survey2/section_template/')
         self.failUnlessEqual(response.status_code, 200)
 
     def test_admin_survey_section_view_add(self):
         """Test Function to check admin surveyquestion add"""
-        response = self.client.get('/admin/survey2/section/add/')
+        response = self.client.get('/admin/survey2/section_template/add/')
         self.failUnlessEqual(response.status_code, 200)
 
     def test_admin_survey_branching_view_list(self):
         """Test Function to check admin surveyresponse list"""
-        response = self.client.get('/admin/survey2/branching/')
+        response = self.client.get('/admin/survey2/branching_template/')
         self.failUnlessEqual(response.status_code, 200)
 
     def test_admin_survey_branching_view_add(self):
         """Test Function to check admin surveyresponse list"""
-        response = self.client.get('/admin/survey2/branching/add/')
+        response = self.client.get('/admin/survey2/branching_template/add/')
         self.failUnlessEqual(response.status_code, 200)
 
 
@@ -76,7 +77,9 @@ class SurveyCustomerView(BaseAuthenticatedClient):
                 'dialer_setting.json', 'phonebook.json', 'contact.json',
                 'campaign.json', 'campaign_subscriber.json',
                 'callrequest.json',
-                'survey.json', 'section.json', 'branching.json',
+                'survey_template.json', 'survey.json',
+                'section_template.json', 'section.json',
+                'branching_template.json', 'branching.json',
                 'user_profile.json']
 
     def test_survey_view_list(self):
@@ -377,6 +380,14 @@ class SurveyCustomerView(BaseAuthenticatedClient):
         response = section_delete(request, 1)
         self.assertEqual(response.status_code, 302)
 
+    def test_survey_view(self):
+        """Test Function survey view"""
+        request = self.factory.get('/survey2_view/1/')
+        request.user = self.user
+        request.session = {}
+        response = survey_view(request, 1)
+        self.assertEqual(response.status_code, 200)
+
     def test_section_phrasing_change(self):
         """Test Function section phrasing update"""
         request = self.factory.get('/section/phrasing/1/')
@@ -452,11 +463,20 @@ class SurveyModel(TestCase):
     fixtures = ['gateway.json', 'auth_user.json', 'contenttype.json',
                 'phonebook.json', 'contact.json',
                 'campaign.json', 'campaign_subscriber.json',
-                'survey.json', 'section.json', 'branching.json',
+                'survey_template.json', 'survey.json',
+                'section_template.json', 'section.json',
+                'branching_template.json', 'branching.json',
                 'callrequest.json']
 
     def setUp(self):
         self.user = User.objects.get(username='admin')
+
+        # Survey_template model
+        self.survey_template = Survey_template(
+            name='test_survey',
+            user=self.user,
+        )
+        self.survey_template.save()
 
         # Survey model
         self.survey = Survey(
@@ -466,6 +486,13 @@ class SurveyModel(TestCase):
         self.survey.save()
         self.assertEqual(self.survey.__unicode__(), u'test_survey')
 
+        # Section_template
+        self.section_template = Section_template(
+            question='test_question',
+            survey=self.survey_template,
+        )
+        self.section_template.save()
+
         # Section model
         self.section = Section(
             question='test_question',
@@ -474,6 +501,12 @@ class SurveyModel(TestCase):
         self.section.save()
         self.assertEqual(self.section.__unicode__(), u'[7] test_question')
 
+        # Branching_template model
+        self.branching_template = Branching_template(
+            keys=5,
+            section=self.section_template,
+        )
+        self.branching_template.save()
 
         # Branching model
         self.branching = Branching(
@@ -508,72 +541,72 @@ class SurveyModel(TestCase):
             self.result_aggregate.__unicode__(), '[1] [7] test_question = apple')
 
     def test_survey_forms(self):
-        self.assertEqual(self.survey.name, "test_survey")
-        self.assertEqual(self.section.survey, self.survey)
-        self.assertEqual(self.branching.section, self.section)
+        self.assertEqual(self.survey_template.name, "test_survey")
+        self.assertEqual(self.section_template.survey, self.survey_template)
+        self.assertEqual(self.branching_template.section, self.section_template)
         self.assertEqual(self.result.section, self.section)
 
-        form = VoiceSectionForm(self.user, instance=self.section)
+        form = VoiceSectionForm(self.user, instance=self.section_template)
         obj = form.save(commit=False)
         obj.question = "test question"
         obj.type = 1
-        obj.survey = self.survey
+        obj.survey_template = self.survey_template
         obj.save()
 
-        form = BranchingForm(self.survey.id, self.section.id)
+        form = BranchingForm(self.survey_template.id, self.section_template.id)
         obj = form.save(commit=False)
         obj.keys = 0
-        obj.section = self.section
-        obj.goto = self.section
+        obj.section_template = self.section_template
+        obj.goto = self.section_template
         obj.save()
 
-        form = MultipleChoiceSectionForm(self.user, instance=self.section)
+        form = MultipleChoiceSectionForm(self.user, instance=self.section_template)
         obj = form.save(commit=False)
         obj.type = 2
         obj.question = "test question"
         obj.key_0 = "apple"
-        obj.survey = self.survey
+        obj.survey_template = self.survey_template
         obj.save()
 
-        form = BranchingForm(self.survey.id, self.section.id)
+        form = BranchingForm(self.survey_template.id, self.section_template.id)
         obj = form.save(commit=False)
         obj.keys = 1
-        obj.section = self.section
-        obj.goto = self.section
+        obj.section_template = self.section_template
+        obj.goto = self.section_template
         obj.save()
 
         form = RatingSectionForm(self.user,
-                                 instance= self.section)
+                                 instance= self.section_template)
         obj = form.save(commit=False)
         obj.type = 3
         obj.question = "test question"
         obj.rating_laps = 5
-        obj.survey = self.survey
+        obj.survey_template = self.survey_template
         obj.save()
 
-        form = BranchingForm(self.survey.id, self.section.id)
+        form = BranchingForm(self.survey_template.id, self.section_template.id)
         obj = form.save(commit=False)
         obj.keys = 2
-        obj.section = self.section
-        obj.goto = self.section
+        obj.section_template = self.section_template
+        obj.goto = self.section_template
         obj.save()
 
         form = EnterNumberSectionForm(self.user,
-                                      instance=self.section)
+                                      instance=self.section_template)
         obj = form.save(commit=False)
         obj.type = 4
         obj.question = "test question"
         obj.number_digits = 2
         obj.min_number = 1
         obj.max_number = 100
-        obj.survey = self.survey
+        obj.survey_template = self.survey_template
         obj.save()
 
         form = BranchingForm(2, 2)
         obj = form.save(commit=False)
         obj.keys = 3
-        obj.section = self.section
-        obj.goto = self.section
+        obj.section_template = self.section_template
+        obj.goto = self.section_template
         obj.save()
 
         form = RecordMessageSectionForm(self.user)
@@ -581,7 +614,7 @@ class SurveyModel(TestCase):
         obj.type = 5
         obj.question = "test question"
         obj.continue_survey = 1
-        obj.survey = self.survey
+        obj.survey_template = self.survey_template
         obj.save()
 
         form = PatchThroughSectionForm(self.user)
@@ -589,21 +622,24 @@ class SurveyModel(TestCase):
         obj.type = 6
         obj.question = "test question"
         obj.dial_phonenumber = 1234567890
-        obj.survey = self.survey
+        obj.survey_template = self.survey_template
         obj.save()
 
         form = PhrasingForm()
         obj = form.save(commit=False)
         obj.phrasing = 'xyz'
-        obj.survey = self.survey
+        obj.survey_template = self.survey_template
         obj.save()
 
         form = SurveyDetailReportForm(self.user,
                                       initial={'campaign': 1})
 
     def teardown(self):
+        self.survey_template.delete()
         self.survey.delete()
+        self.section_template.delete()
         self.section.delete()
+        self.branching_template.delete()
         self.branching.delete()
         self.result.delete()
         self.result_aggregate.delete()

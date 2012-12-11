d5a8b98144759297c9183c64d641cc192006fd46
Areski Belaid <areski@gmail.com>
2012-12-11 12:24:33 +0100

    update lua

--- lua/libs/example.lua
+++ lua/libs/example.lua
@@ -1,8 +1,37 @@
 --
--- Test & demonstration file for "fsm.lua" 
+-- Test & demonstration file for "fsm.lua"
 --
 
 FSM = require "fsm"
+
+
+function action1() print("Performing action 1") end
+function action2() print("Performing action 2") end
+
+-- Define your state transitions here
+local myStateTransitionTable = {
+    {"state1", "event1", "state2", action1},
+    {"state2", "event2", "state3", action2},
+    {"*",      "event3", "state2", action1},  -- for any state
+    {"*",      "*",      "state2", action2}   -- exception handler
+}
+
+-- Create your finite state machine
+fsm = FSM.new(myStateTransitionTable)
+
+-- Use your finite state machine
+-- which starts by default with the first defined state
+print("Current FSM state: " .. fsm:get())
+
+-- Or you can set another state
+fsm:set("state2")
+print("Current FSM state: " .. fsm:get())
+
+-- Resond on "event" and last set "state"
+fsm:fire("event3")
+print("Current FSM state: " .. fsm:get())
+
+
 
 function action1() print("Performing action 1") end
 
@@ -24,12 +53,12 @@ fsm = FSM.new(myStateTransitionTable)
 -- print( "Constant UNKNOWN = " .. UNKNOWN )
 print( "Constant FSM.UNKNOWN = " .. FSM.UNKNOWN )
 
--- Use your finite state machine 
+-- Use your finite state machine
 -- which starts by default with the first defined state
 print("Current FSM state: " .. fsm:get())
 
 -- Or you can set another state
-fsm:set("state2")							
+fsm:set("state2")
 print("Current FSM state: " .. fsm:get())
 
 -- Respond on "event" and last set "state"
@@ -41,34 +70,34 @@ print("Force exception by firing unknown event")
 fsm:fire("event3")
 print("Current FSM state: " .. fsm:get())
 
--- Test automated exception handling
-local myStateTransitionTable2 = {
-	{"state1", "event1", "state2", action1},
-	{"state2", "event2", "state3", action2},
-	{"*"     , "event3", "state1", action4},
-	{"*"     , "?",      "state1", action3}
-}
+-- -- Test automated exception handling
+-- local myStateTransitionTable2 = {
+-- 	{"state1", "event1", "state2", action1},
+-- 	{"state2", "event2", "state3", action2},
+-- 	{"*"     , "event3", "state1", action4},
+-- 	{"*"     , "?",      "state1", action3}
+-- }
 
--- Create your instance of a finite state machine
-fsm2 = FSM.new(myStateTransitionTable2)
-fsm2:set("state2")	
-print("\nCurrent FSM-2 state: " .. fsm2:get())
+-- -- Create your instance of a finite state machine
+-- fsm2 = FSM.new(myStateTransitionTable2)
+-- fsm2:set("state2")
+-- print("\nCurrent FSM-2 state: " .. fsm2:get())
 
-fsm2:delete({{"state2", "event2"}} )
--- Force exception
-fsm2:fire("event2")
+-- fsm2:delete({{"state2", "event2"}} )
+-- -- Force exception
+-- fsm2:fire("event2")
 
-fsm2:delete({{"*", "?"}} )
-print("Force third exception (silence = true)")
-fsm2:silent() -- prevent unknown state-event notificaton to be printed
+-- fsm2:delete({{"*", "?"}} )
+-- print("Force third exception (silence = true)")
+-- fsm2:silent() -- prevent unknown state-event notificaton to be printed
 
-fsm2:fire("event3")
-print("Current FSM-2 state after firing  wildcard 'event3': " .. fsm2:get())
+-- fsm2:fire("event3")
+-- print("Current FSM-2 state after firing  wildcard 'event3': " .. fsm2:get())
 
-fsm2:add({{"*"     , "*",      "state2", action3}})
-fsm2:fire("event2")
-print("Current FSM-2 state: " .. fsm2:get())
-print("Current FSM state: " .. fsm:get())
+-- fsm2:add({{"*"     , "*",      "state2", action3}})
+-- fsm2:fire("event2")
+-- print("Current FSM-2 state: " .. fsm2:get())
+-- print("Current FSM state: " .. fsm:get())
 
 --[[
 Expected output:

92103d712f6346826703a92bf62b221568c4e94d
Areski Belaid <areski@gmail.com>
2012-12-18 12:56:05 +0100

    work in progress on listener for CDRs

--- lua/listener.lua
+++ lua/listener.lua
@@ -1,4 +1,8 @@
 
+
+-- List of Channels States
+-- http://wiki.freeswitch.org/wiki/Event_list#CHANNEL_STATE
+
 local luasql = require "luasql.postgres"
 
 -- Database setup
@@ -23,6 +27,13 @@ function sleep(seconds)
     while os.clock()-time < seconds do end
 end
 
+function trim(s)
+    --trim text
+    if s == nil then
+        return ''
+    end
+    return (string.gsub(s, "^%s*(.-)%s*$", "%1"))
+end
 
 if argv[1] then
     i=1
@@ -47,8 +58,17 @@ logger("Starting")
 local event_name
 local event_subclass
 
-con = freeswitch.EventConsumer("all") -- too lazy to figure out why "heartbeat CUSTOM lua::stop_event"
-                                 -- does not work properly, that is really all we need
+
+con = freeswitch.EventConsumer("ALL")
+-- con = freeswitch.EventConsumer("CHANNEL_CALLSTATE")
+-- con = freeswitch.EventConsumer("CHANNEL_CREATE")
+-- con = freeswitch.EventConsumer("CHANNEL_PROGRESS")
+-- con = freeswitch.EventConsumer("CHANNEL_PROGRESS_MEDIA")
+-- con = freeswitch.EventConsumer("CHANNEL_HANGUP_COMPLETE")
+-- con = freeswitch.EventConsumer("BACKGROUND_JOB")
+-- -- con = freeswitch.EventConsumer("CHANNEL_STATE")
+-- con = freeswitch.EventConsumer("CUSTOM lua::stop_event")
+
 api = freeswitch.API()
 
 hostname = api:execute("hostname")
@@ -57,7 +77,7 @@ i = 0
 while true do
     event_name = false
     event_subclass = false
-    if i > 100 then
+    if i > 50 then
         break
     end
     i = i + 1
@@ -66,6 +86,7 @@ while true do
     -- pop(1,500) blocks for max half a second until there is an event
     e = con:pop(1)
 
+    -- 0. Check Background job -> assign uuid to callrequest
     -- 1. Get Hangup even
     -- 2. Check vars : if flag Newfies call
     -- 3. Update Call Status if started
@@ -74,11 +95,54 @@ while true do
 
 
     if e then
+        -- Get the Events info
         event_name = e:getHeader("Event-Name") or ""
         event_subclass = e:getHeader("Event-Subclass") or ""
-        logger('-----------------------------')
-        logger(event_name)
-        logger(event_subclass)
+
+        if event_name == 'CHANNEL_CREATE' or event_name == 'CHANNEL_HANGUP_COMPLETE' or event_name == 'BACKGROUND_JOB' or event_name == 'CHANNEL_ANSWER' then
+
+            logger('-----------------------------')
+            logger(event_name)
+            logger(event_subclass)
+
+            if event_name == 'BACKGROUND_JOB' then
+                variable_newfiesdialer = e:getHeader("variable_newfiesdialer")
+                if variable_newfiesdialer ~= nil then
+                    logger("variable_newfiesdialer is: " .. variable_newfiesdialer .. "\n")
+                end
+                data = e:getBody()
+                if data ~= nil then
+                    if string.len(data) > 10 then
+                        if string.sub(data, 0, 4) == '-ERR' then
+                            logger("ERROR OUTBOUND: " .. string.sub(data, 5) .. "(END)\n")
+                        end
+                    end
+                    logger("Here's getBody: " .. trim(data) .. "(END)\n")
+                end
+            end
+
+            if event_name == 'CHANNEL_HANGUP_COMPLETE' then
+                logger('************************************')
+                variable_newfiesdialer = e:getHeader("variable_newfiesdialer") or ""
+                logger(variable_newfiesdialer)
+                logger('************************************')
+                if variable_newfiesdialer ~= nil then
+                    logger("variable_newfiesdialer is: " .. variable_newfiesdialer .. "\n")
+                end
+            end
+
+            data = e:getBody()
+            if data ~= nil then
+                logger("Here's getBody:\n" .. trim(data) .. "\n")
+            end
+            --logger("Here's everything:\n" .. data .. "\n")
+
+            -- Grab a specific channel variable
+            uuid = e:getHeader("uuid")
+            if uuid ~= nil then
+                logger("uuid is: " .. uuid .. "\n")
+            end
+        end
 
         --load = api:execute("status")
 

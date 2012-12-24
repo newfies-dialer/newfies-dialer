36c407ab230f612a32dab37c35f1c03384314198
Areski Belaid <areski@gmail.com>
2012-12-24 16:00:51 +0100

    Fix display of hangup on branching / lazy gettext

--- newfies/dialer_cdr/templatetags/dialer_cdr_extras.py
+++ newfies/dialer_cdr/templatetags/dialer_cdr_extras.py
@@ -233,7 +233,8 @@ def get_branching_goto_field(section_id, selected_value):
     get_branching_goto_field
     """
     section_obj = Section_template.objects.get(id=section_id)
-    option_list = '<option value="">%s</option>' % _('Hang up')
+    #We don't need a lazy translation in this case
+    option_list = '<option value="">%s</option>' % _('Hang up').encode('utf-8')
     list = Section_template.objects.filter(survey_id=section_obj.survey_id).order_by('id')
     for i in list:
         if i.question:

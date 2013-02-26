719e5fafbd6bf25f54ad530d56b4563e1eedb585
Areski Belaid <areski@gmail.com>
2013-02-26 18:00:03 +0100

    update header

--- lua/libs/lfs_cache.lua
+++ lua/libs/lfs_cache.lua
@@ -1,16 +1,33 @@
 --
--- Newfies-Dialer License
--- http://www.newfies-dialer.org
+-- Easily power a caching system based on Filesystem
+-- It's recommended to use ramdisk to get better performances
+-- @ mkdir -p /tmp/ram
+-- @ sudo mount -t tmpfs -o size=512M tmpfs /tmp/ram/
+-- Set the CACHE_DIRECTORY setting to this directory
 --
--- This Source Code Form is subject to the terms of the Mozilla Public
--- License, v. 2.0. If a copy of the MPL was not distributed with this file,
--- You can obtain one at http://mozilla.org/MPL/2.0/.
 --
--- Copyright (C) 2011-2013 Star2Billing S.L.
+-- Copyright (C) 2013 Arezqui Belaid <areski@gmail.com>
 --
--- The Initial Developer of the Original Code is
--- Arezqui Belaid <info@star2billing.com>
+-- Permission is hereby granted, free of charge, to any person
+-- obtaining a copy of this software and associated documentation files
+-- (the "Software"), to deal in the Software without restriction,
+-- including without limitation the rights to use, copy, modify, merge,
+-- publish, distribute, sublicense, and/or sell copies of the Software,
+-- and to permit persons to whom the Software is furnished to do so,
+-- subject to the following conditions:
 --
+-- The above copyright notice and this permission notice shall be
+-- included in all copies or substantial portions of the Software.
+--
+-- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+-- EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+-- MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+-- NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+-- BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+-- ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+-- CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+-- SOFTWARE.
+
 
 require "lfs"
 require "md5"
@@ -105,20 +122,11 @@ end
 
 
 --
--- test
+-- run test
+--
 if false then
 
     caching = LFS_Caching(nil)
-
-    -- print("get Cache")
-    -- res = caching:get('mykey', 10)
-    -- print(res)
-    -- if not(res) then
-    --     print("Set Cache")
-    --     caching:set('mykey', 12345)
-    --     res = 12345
-    --     print(res)
-    -- end
 
     local cmsgpack = require 'cmsgpack'
     local inspect = require 'inspect'
@@ -131,15 +139,12 @@ if false then
     msgpack = cmsgpack.pack(value_test)
     print(msgpack)
 
-    print("get Cache")
+    print("Test Get Cache")
     res = caching:get('hashkeydb', 3)
-    print("~~~~~~~~~~~~~~~~~~~~~~~~~")
     if not(res) then
         print("Set Cache")
         caching:set('hashkeydb', msgpack)
     else
-        print("res ==> "..res)
-        print("UNPACK RESUlt => ")
         print(inspect(cmsgpack.unpack(res)))
     end
 end

23e75b324b16aeb1662abb249b86cccd48c8faf6
Shrenik Patel <shrenik181986@gmail.com>
2013-03-27 15:55:13 +0530

    fix agent admin form

--- newfies/agent/admin.py
+++ newfies/agent/admin.py
@@ -12,59 +12,15 @@
 # Arezqui Belaid <info@star2billing.com>
 #
 
-from django import forms
 from django.contrib import admin
-from django.contrib.auth.models import User
 from django.contrib.auth.admin import UserAdmin
-from django.utils.translation import ugettext as _
-from django.contrib.auth.forms import UserChangeForm, UserCreationForm
-from django.db.models import Q
 from agent.models import Agent, AgentProfile
-from user_profile.models import Manager
+from agent.forms import AgentProfileForm
 
-
-def manager_list():
-    manager_list = []
-    list = Manager.objects.values_list('id', 'username')\
-        .filter(is_staff=True, is_superuser=False, is_active=True).order_by('id')
-    for l in list:
-        manager_list.append((l[0], l[1]))
-    return manager_list
-
-
-"""
-class AgentCreationForm(UserCreationForm):
-    class Meta:
-        model = Agent
-        fields = ("username", "password1", "password2")
-
-
-class AgentChangeForm(UserChangeForm):
-    is_agent = forms.BooleanField()
-    manager = forms.ChoiceField(label=_("manager"),
-        choices=manager_list())
-
-    class Meta:
-        model = Agent
-"""
-
-"""
-class AgentAdmin(UserAdmin):
-    form = AgentChangeForm
-    add_form = AgentCreationForm
-
-    fieldsets = (
-        (None, {'fields': ('username', 'password')}),
-        (_('Personal info'), {'fields': ('is_agent', 'manager', 'first_name', 'last_name', 'email')}),
-        # (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',
-        #                                'groups', 'user_permissions')}),
-        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
-    )
-    list_filter = []
-"""
 
 class AgentProfileInline(admin.StackedInline):
     model = AgentProfile
+    form = AgentProfileForm
 
 
 class AgentAdmin(UserAdmin):
--- newfies/agent/forms.py
+++ newfies/agent/forms.py
@@ -11,4 +11,27 @@
 # The Initial Developer of the Original Code is
 # Arezqui Belaid <info@star2billing.com>
 #
+from django import forms
+from django.forms import ModelForm
+from django.utils.translation import ugettext as _
+from agent.models import AgentProfile
+from user_profile.models import Manager
 
+
+def manager_list():
+    manager_list = []
+    list = Manager.objects.values_list('id', 'username')\
+        .filter(is_staff=True, is_superuser=False, is_active=True).order_by('id')
+    for l in list:
+        manager_list.append((l[0], l[1]))
+    return manager_list
+
+
+class AgentProfileForm(ModelForm):
+
+    class Meta:
+        model = AgentProfile
+
+    def __init__(self, *args, **kwargs):
+        super(AgentProfileForm, self).__init__(*args, **kwargs)
+        self.fields['manager'].choices = manager_list()

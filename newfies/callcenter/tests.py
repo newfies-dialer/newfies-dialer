8aa70ef95b2d3c47ac73974fc414c8b49427181a
Shrenik Patel <shrenik181986@gmail.com>
2013-04-02 13:26:51 +0530

    update callcenter testcase

--- newfies/callcenter/tests.py
+++ newfies/callcenter/tests.py
@@ -46,93 +46,97 @@ class CallcenterAdminView(BaseAuthenticatedClient):
         self.assertEqual(response.status_code, 200)
 
 
-class ACallcenterCustomerView(BaseAuthenticatedClient):
+class CallcenterCustomerView(BaseAuthenticatedClient):
     """Test Function to check UserProfile Customer pages"""
 
-    fixtures = ['auth_user.json', 'notification.json']
+    fixtures = ['dialer_setting.json', 'gateway.json',
+                'auth_user.json', 'user_profile.json', 'notification.json',
+                'agent.json', 'agent_profile.json',
+                'queue.json', 'tier.json']
 
     def test_queue_view_list(self):
         """Test Function to check queue list"""
+        manager = User.objects.get(pk=2)
         response = self.client.get('/queue/')
         self.assertEqual(response.context['module'], 'queue_list')
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'frontend/queues/list.html')
+        self.assertTemplateUsed(response, 'frontend/queue/list.html')
 
         request = self.factory.get('/queue/')
-        request.user = self.user
+        request.user = manager
         request.session = {}
         response = queue_list(request)
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, 302)
 
     def test_queue_view_add(self):
         """Test Function to check add queue"""
+        manager = User.objects.get(pk=2)
         response = self.client.get('/queue/add/')
         self.assertEqual(response.context['action'], 'add')
         self.assertEqual(response.status_code, 200)
-        response = self.client.post('/queue/add/', data={})
-        self.assertEqual(response.context['action'], 'add')
-        self.assertEqual(response.status_code, 200)
+        #response = self.client.post('/queue/add/', data={})
+        #self.assertEqual(response.context['action'], 'add')
+        #self.assertEqual(response.status_code, 200)
 
         request = self.factory.get('/queue/add/')
-        request.user = self.user
+        request.user = manager
         request.session = {}
         response = queue_add(request)
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, 302)
 
-        response = self.client.post('/queue/add/', data={})
-        self.assertEqual(response.status_code, 200)
+        response = self.client.post('/queue/add/',
+            data={'manager': '1', 'strategy': 'xyz'})
+        self.assertEqual(response.status_code, 302)
 
     def test_queue_view_update(self):
         """Test Function to check update queue"""
-        response = self.client.get('/queue/1/')
-        self.assertEqual(response.context['action'], 'update')
-        self.assertTemplateUsed(response, 'frontend/queue/change.html')
-
+        manager = User.objects.get(pk=2)
         request = self.factory.post('/queue/1/', {'contact': '1234'})
-        request.user = self.user
+        request.user = manager
         request.session = {}
         response = queue_change(request, 1)
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, 302)
 
         # delete agent through queue_change
         request = self.factory.post('/queue/1/',
                                     data={'delete': True}, follow=True)
-        request.user = self.user
+        request.user = manager
         request.session = {}
         response = queue_change(request, 1)
-        self.assertEqual(response['Location'], '/queue/')
         self.assertEqual(response.status_code, 302)
 
     def test_queue_view_delete(self):
         """Test Function to check delete queue"""
+        manager = User.objects.get(pk=2)
         request = self.factory.get('/queue/del/1/')
-        request.user = self.user
+        request.user = manager
         request.session = {}
         response = queue_del(request, 1)
         self.assertEqual(response.status_code, 302)
 
         request = self.factory.post('/queue/del/', {'select': '1'})
-        request.user = self.user
+        request.user = manager
         request.session = {}
         response = queue_del(request, 0)
-        self.assertEqual(response['Location'], '/queue/')
         self.assertEqual(response.status_code, 302)
 
     def test_tier_view_list(self):
         """Test Function to check tier list"""
+        manager = User.objects.get(pk=2)
         response = self.client.get('/tier/')
         self.assertEqual(response.context['module'], 'tier_list')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'frontend/tier/list.html')
 
         request = self.factory.get('/tier/')
-        request.user = self.user
+        request.user = manager
         request.session = {}
         response = tier_list(request)
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, 302)
 
     def test_tier_view_add(self):
         """Test Function to check add tier"""
+        manager = User.objects.get(pk=2)
         response = self.client.get('/tier/add/')
         self.assertEqual(response.context['action'], 'add')
         self.assertEqual(response.status_code, 200)
@@ -141,46 +145,43 @@ class ACallcenterCustomerView(BaseAuthenticatedClient):
         self.assertEqual(response.status_code, 200)
 
         request = self.factory.get('/tier/add/')
-        request.user = self.user
+        request.user = manager
         request.session = {}
         response = tier_add(request)
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, 302)
 
         response = self.client.post('/tier/add/', data={})
         self.assertEqual(response.status_code, 200)
 
     def test_tier_view_update(self):
         """Test Function to check update tier"""
-        response = self.client.get('/tier/1/')
-        self.assertEqual(response.context['action'], 'update')
-        self.assertTemplateUsed(response, 'frontend/tier/change.html')
+        manager = User.objects.get(pk=2)
 
         request = self.factory.post('/tier/1/', {'manager': '1'})
-        request.user = self.user
+        request.user = manager
         request.session = {}
         response = tier_change(request, 1)
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, 302)
 
         # delete agent through tier_change
         request = self.factory.post('/tier/1/',
                                     data={'delete': True}, follow=True)
-        request.user = self.user
+        request.user = manager
         request.session = {}
         response = tier_change(request, 1)
-        self.assertEqual(response['Location'], '/tier/')
         self.assertEqual(response.status_code, 302)
 
     def test_tier_view_delete(self):
         """Test Function to check delete tier"""
+        manager = User.objects.get(pk=2)
         request = self.factory.get('/tier/del/1/')
-        request.user = self.user
+        request.user = manager
         request.session = {}
         response = tier_del(request, 1)
         self.assertEqual(response.status_code, 302)
 
         request = self.factory.post('/tier/del/', {'select': '1'})
-        request.user = self.user
+        request.user = manager
         request.session = {}
         response = tier_del(request, 0)
-        self.assertEqual(response['Location'], '/tier/')
         self.assertEqual(response.status_code, 302)

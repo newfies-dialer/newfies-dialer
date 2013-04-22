5ac93539c01b15b7c4866c0c2dbd1e2f6c8c37b5
Areski Belaid <areski@gmail.com>
2013-04-22 18:14:29 +0200

    change localhost to 127.0.0.1 for ESL connection

--- newfies/settings.py
+++ newfies/settings.py
@@ -453,7 +453,7 @@ AUDIO_DEBUG = False
 
 #ESL
 #===
-ESL_HOSTNAME = 'localhost'
+ESL_HOSTNAME = '127.0.0.1'
 ESL_PORT = '8021'
 ESL_SECRET = 'ClueCon'
 ESL_SCRIPT = '&lua(/usr/share/newfies-lua/newfies.lua)'

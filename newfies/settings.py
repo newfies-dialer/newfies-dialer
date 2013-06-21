8abd1bc94c44dd6791fdf03be16c83aaa02b1c1d
Areski Belaid <areski@gmail.com>
2013-06-21 13:29:23 +0200

    add django-debug-toolbar-template-timings

--- newfies/settings.py
+++ newfies/settings.py
@@ -166,7 +166,6 @@ TEMPLATE_DIRS = (
 
 INTERNAL_IPS = ('127.0.0.1',)
 
-#ALLOWED_HOSTS
 ALLOWED_HOSTS = ['127.0.0.1']
 
 DAJAXICE_MEDIA_PREFIX = "dajaxice"
@@ -244,11 +243,25 @@ try:
 except ImportError:
     pass
 else:
-    INSTALLED_APPS = INSTALLED_APPS + ('debug_toolbar',)
+    INSTALLED_APPS = INSTALLED_APPS + ('debug_toolbar', 'template_timings_panel',)
     MIDDLEWARE_CLASSES = MIDDLEWARE_CLASSES + \
         ('debug_toolbar.middleware.DebugToolbarMiddleware',)
+    DEBUG_TOOLBAR_PANELS = (
+        'debug_toolbar.panels.version.VersionDebugPanel',
+        'debug_toolbar.panels.timer.TimerDebugPanel',
+        'debug_toolbar.panels.settings_vars.SettingsVarsDebugPanel',
+        'debug_toolbar.panels.headers.HeaderDebugPanel',
+        'debug_toolbar.panels.request_vars.RequestVarsDebugPanel',
+        'debug_toolbar.panels.template.TemplateDebugPanel',
+        'debug_toolbar.panels.sql.SQLDebugPanel',
+        'debug_toolbar.panels.signals.SignalDebugPanel',
+        'debug_toolbar.panels.logger.LoggingPanel',
+        'template_timings_panel.panels.TemplateTimings.TemplateTimings',
+    )
     DEBUG_TOOLBAR_CONFIG = {
         'INTERCEPT_REDIRECTS': False,
+        'HIDE_DJANGO_SQL': False,
+        'ENABLE_STACKTRACES': True,
     }
 
 # Django extensions

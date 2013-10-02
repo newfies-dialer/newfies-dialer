6fcc6174d33316376e068aea429d4fb888f55dcc
Shrenik Patel <shrenik181986@gmail.com>
2013-10-02 19:01:48 +0530

    update user_dialer_settings.html

--- newfies/dialer_settings/admin.py
+++ newfies/dialer_settings/admin.py
@@ -25,10 +25,10 @@ class DialerSettingAdmin(admin.ModelAdmin):
     of a DialerSetting."""
     list_display = ('name', 'max_frequency', 'callmaxduration', 'maxretry',
                     'max_calltimeout', 'max_number_campaign',
-                    'max_number_subscriber_campaign', 'blacklist', 'whitelist',
-                    'updated_date')
+                    'max_number_subscriber_campaign', 'max_number_contact',
+                    'blacklist', 'whitelist', 'updated_date')
     #list_filter = ['setting_group']
     search_fields = ('name', )
-    ordering = ('-name', )    
+    ordering = ('-name', )
 
 admin.site.register(DialerSetting, DialerSettingAdmin)

a7f711ce18646735c6c08e23db07418a338f0b8f
Jonathan Roper <joe.roper@gmail.com>
2013-10-17 14:31:25 +0200

    install Node & npm
    yum -y --enablerepo=epel install npm

--- install/newfies-dialer-functions.sh
+++ install/newfies-dialer-functions.sh
@@ -278,7 +278,11 @@ func_install_dependencies(){
 
             #Install Supervisor
             pip install supervisor
-
+            
+            # install Node & npm
+            yum -y --enablerepo=epel install npm
+            
+              
             #Audio File Conversion
             yum -y --enablerepo=rpmforge install sox sox-devel ffmpeg ffmpeg-devel mpg123 mpg123-devel libmad libmad-devel libid3tag libid3tag-devel lame lame-devel flac-devel libvorbis-devel
             cd /usr/src/

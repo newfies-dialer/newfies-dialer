e8d3732b0555516ca761dc0815b65ec691ef8972
Shrenik Patel <shrenik181986@gmail.com>
2013-11-07 19:38:55 +0530

    fix import on appointment task + remove Occurrence from admin

--- newfies/appointment/models/__init__.py
+++ newfies/appointment/models/__init__.py
@@ -2,4 +2,5 @@ from appointment.models.calendars import *
 from appointment.models.events import *
 from appointment.models.alarms import *
 from appointment.models.rules import *
+from appointment.models.users import *
 from appointment.signals import *

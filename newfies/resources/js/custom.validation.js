b643f178b95bb8a9c11dcaf1525d8c1fd8f114b8
Areski Belaid <areski@gmail.com>
2013-12-05 19:07:32 +0100

    remove jquery-ui

--- newfies/resources/js/custom.validation.js
+++ newfies/resources/js/custom.validation.js
@@ -1,10 +1,10 @@
 
 function numeric(event)
-{       
+{
 	// Allow only backspace and delete
 	if ( event.keyCode == 46 || event.keyCode == 8 ) {
 		// let it happen, don't do anything
-	
+
 	// Ensure that it is a number and stop the keypress
 	}else if ((event.keyCode >= 48 && event.keyCode <= 57) || (event.keyCode >= 96 && event.keyCode <= 105)) {
 

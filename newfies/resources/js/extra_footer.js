9340603a51e5a08383dd177a1a62363f8cec66f7
Shrenik Patel <shrenik181986@gmail.com>
2014-01-16 11:45:20 +0530

    remove newfies_custom_js.html

--- newfies/resources/js/extra_footer.js
+++ newfies/resources/js/extra_footer.js
@@ -2,3 +2,36 @@ $(function() {
   // initialize all the inputs
   $('.make-switch>input[type="checkbox"],[type="radio"]').bootstrapSwitch();
 });
+
+
+$(function () {
+    $('#helpover').popover();
+    $(".fg-button").hover(
+        function(){
+            $(this).addClass("ui-state-hover");
+        },
+        function(){
+            $(this).removeClass("ui-state-hover");
+        }
+    );
+    $('#buttonlogin').click(function(){
+      $("#requestlogin").animate({ height: 'show', opacity: 'show' }, 'slow');
+    });
+    $("#language-container").change(function() {
+        this.form.submit();
+    });
+});
+
+// toggle event for forms
+$(function() {
+
+    $("#form_collapse").on('hidden.bs.collapse', function() {
+        $('#toggle_btn_i_span').text('{% trans "open search"|title %}');
+        $('#toggle_btn_i').attr('class', 'glyphicon glyphicon-zoom-in');
+    });
+
+    $("#form_collapse").on('shown.bs.collapse', function() {
+        $('#toggle_btn_i_span').text('{% trans "hide search"|title %}');
+        $('#toggle_btn_i').attr('class', 'glyphicon glyphicon-zoom-out');
+    });
+});

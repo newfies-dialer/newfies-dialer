55ba20050f8ef4e4c480f2053c4566b566e058d4
Shrenik Patel <shrenik181986@gmail.com>
2014-01-28 13:52:33 +0530

    extra_footer.js code moved to newfies_custom.js

--- newfies/resources/js/newfies_custom.js
+++ newfies/resources/js/newfies_custom.js
@@ -218,3 +218,43 @@ if(document.location.href.search("/calendar/") != -1 || document.location.href.s
        $('#id_start_picker').data("DateTimePicker").setEndDate(e.date);
     });
 }
+
+/// toggle code///
+/// convert checkbox into switch button ///
+$(function() {
+  // initialize all the inputs
+  $('.make-switch>input[type="checkbox"],[type="radio"]').bootstrapSwitch();
+});
+
+
+$(function () {
+    $('#helpover').popover();
+    $(".fg-button").hover(
+        function(){
+            $(this).addClass("ui-state-hover");
+        },
+        function(){
+            $(this).removeClass("ui-state-hover");
+        }
+    );
+    $('#buttonlogin').click(function(){
+      $("#requestlogin").animate({ height: 'show', opacity: 'show' }, 'slow');
+    });
+    $("#language-container").change(function() {
+        this.form.submit();
+    });
+});
+
+// toggle event for hide/open search button
+$(function() {
+
+    $("#form_collapse").on('hidden.bs.collapse', function() {
+        $('#toggle_btn_i_span').text(gettext('open search'));
+        $('#toggle_btn_i').attr('class', 'glyphicon glyphicon-zoom-in');
+    });
+
+    $("#form_collapse").on('shown.bs.collapse', function() {
+        $('#toggle_btn_i_span').text(gettext('hide search'));
+        $('#toggle_btn_i').attr('class', 'glyphicon glyphicon-zoom-out');
+    });
+});

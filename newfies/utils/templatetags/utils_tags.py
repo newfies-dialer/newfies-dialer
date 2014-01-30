cd5c3e97379cc0df974bda58fed654793a71af03
Shrenik Patel <shrenik181986@gmail.com>
2014-01-30 11:25:01 +0530

    improve field_html_code tag

--- newfies/utils/templatetags/utils_tags.py
+++ newfies/utils/templatetags/utils_tags.py
@@ -31,26 +31,41 @@ def striphtml(data):
 
 
 @register.simple_tag(name='field_html_code')
-def field_html_code(field, col_class_1='col-md-6', col_class_2='col-xs-8'):
+def field_html_code(field, col_class_1='col-md-6', col_class_2='col-xs-8', flag_error_text=True, flag_help_text=True):
     """
     Usage: {% field_html_code field 'col-md-6' 'col-xs-8' %}
     """
-    error_class = ''
-    if field.errors:
-        error_class = 'has-error'
+    div_string = ''
+    # TODO : country field in Update Calendar User creating problem error
+    # UnicodeDecodeError at /module/calendar_user/x/
+    # 'ascii' codec can't decode byte 0xc3 in position 188: ordinal not in range(128)
 
-    div_string = '<div class="%s">\n' % col_class_1
-    div_string += stab(1) + '<div class="form-group %s">\n' % (error_class)
-    div_string += stab(2) + '<div class="%s">\n' % col_class_2
-    div_string += stab(3) + '<label class="control-label" for="%s">%s</label>\n' % (
-        field.auto_id, word_capital(field.label))
-    div_string += stab(3) + '%s\n' % field
+    if field.name != 'country':
+        if col_class_1:
+            div_string = '<div class="%s">\n' % col_class_1
 
-    if field.errors:
-        div_string += stab(3) + '<span class="help-block">%s</span>\n' % striphtml(str(field.errors)).capitalize()
+        div_string += stab(1) + '<div class="form-group %s">\n' % (
+            'has-error' if field.errors else '')
 
-    div_string += stab(3) + '<span class="help-block">%s</span>\n' % (field.help_text.capitalize())
-    div_string += stab(2) + '</div>\n'
-    div_string += stab(1) + '</div>\n'
-    div_string += '</div>\n'
+        if col_class_2:
+            div_string += stab(2) + '<div class="%s">\n' % col_class_2
+
+        div_string += stab(3) + '<label class="control-label" for="%s">%s</label>\n' % (
+            field.auto_id, word_capital(field.label))
+        div_string += stab(3) + '%s\n' % (field)
+
+        if field.errors and flag_error_text:
+            div_string += stab(3) + '<span class="help-block">%s</span>\n' % striphtml(str(field.errors)).capitalize()
+
+        if flag_help_text:
+            div_string += stab(3) + '<span class="help-block">%s</span>\n' % (field.help_text.capitalize())
+
+        div_string += stab(2) + '</div>\n'
+
+        if col_class_2:
+            div_string += stab(1) + '</div>\n'
+
+        if col_class_1:
+            div_string += '</div>\n'
+
     return mark_safe(div_string)

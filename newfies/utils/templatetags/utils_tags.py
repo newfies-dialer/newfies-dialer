3d34a1d6d944e6ca215b7adfceebb3659f39baa9
areski <areski@gmail.com>
2014-01-30 17:02:17 +0100

    improving template tag - add field_html_code2

--- newfies/utils/templatetags/utils_tags.py
+++ newfies/utils/templatetags/utils_tags.py
@@ -16,6 +16,7 @@ from django.utils.safestring import mark_safe
 from django.template.defaultfilters import register
 from common.common_functions import word_capital
 import re
+from string import Template
 
 
 def stab(tab=1):
@@ -36,34 +37,62 @@ def field_html_code(field, col_class_1='col-md-6', col_class_2='col-xs-8', flag_
     Usage: {% field_html_code field 'col-md-6' 'col-xs-8' %}
     """
     div_string = ''
-    if col_class_1:
-        div_string = '<div class="%s">\n' % col_class_1
+    if col_class_1 or col_class_2:
+        div_string = '<div class="%s %s">\n' % (col_class_1, col_class_2)
 
-    div_string += stab(1) + '<div class="form-group %s">\n' % (
+    div_string += '<div class="form-group %s">\n' % (
         'has-error' if field.errors else '')
 
-    if col_class_2:
-        div_string += stab(2) + '<div class="%s">\n' % col_class_2
-
-    div_string += stab(3) + '<label class="control-label" for="%s">%s</label>\n' % (
+    div_string += '<label class="control-label" for="%s">%s</label>\n' % (
         field.auto_id, word_capital(field.label))
-    div_string += stab(3) + '%s\n' % (field)
+
+    div_string += '%s\n' % str(field).decode("utf-8")
 
     if field.errors and flag_error_text:
-        div_string += stab(3) + '<span class="help-block">%s</span>\n' % striphtml(str(field.errors)).capitalize()
+        div_string += '<span class="help-block">%s</span>\n' % striphtml(str(field.errors)).capitalize()
 
     if flag_help_text:
-        div_string += stab(3) + '<span class="help-block">%s</span>\n' % (field.help_text.capitalize())
+        div_string += '<span class="help-block">%s</span>\n' % (field.help_text.capitalize())
 
-    div_string += stab(2) + '</div>\n'
+    div_string += '</div>\n'
 
-    if col_class_2:
-        div_string += stab(1) + '</div>\n'
-
-    if col_class_1:
+    if col_class_1 or col_class_2:
         div_string += '</div>\n'
 
     return mark_safe(div_string)
+
+
+@register.simple_tag(name='field_html_code2')
+def field_html_code2(field, main_class='col-md-6 col-xs-8', flag_error_text=True, flag_help_text=True):
+    """
+    Usage: {% field_html_code field 'col-md-6' 'col-xs-8' %}
+    """
+    tmp_div = Template("""
+        <div class="$main_class">
+            <div class="form-group $has_error">
+                <label class="control-label" for="$field_auto_id">$field_label</label>
+                $field
+                $field_errors
+                $field_help_test
+            </div>
+        </div>
+    """)
+    has_error = 'has-error' if field.errors else ''
+    if field.errors and flag_error_text:
+        field_errors = '<span class="help-block">%s</span>\n' % striphtml(str(field.errors)).capitalize()
+    else:
+        field_errors = ''
+    if flag_help_text:
+        field_help_test = '<span class="help-block">%s</span>\n' % (field.help_text.capitalize())
+    else:
+        field_help_test = ''
+    htmlcell = tmp_div.substitute(
+        main_class=main_class, has_error=has_error,
+        field_auto_id=field.auto_id, field_label=word_capital(field.label),
+        field=str(field).decode("utf-8"), field_errors=field_errors,
+        field_help_test=field_help_test)
+
+    return mark_safe(htmlcell)
 
 
 @register.simple_tag(name='search_form_field_html_code')

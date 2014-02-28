22527bc9caf65ea7b90f242963ed999d85d8a60b
areski <areski@gmail.com>
2014-02-28 11:55:57 +0100

    fix pep

--- newfies/newfies_dialer/urls.py
+++ newfies/newfies_dialer/urls.py
@@ -61,10 +61,7 @@ urlpatterns = patterns('',
 
 if settings.DEBUG:
     import debug_toolbar
-    urlpatterns += patterns('',
-        url(r'^__debug__/', include(debug_toolbar.urls)),
-    )
-
+    urlpatterns += patterns('', url(r'^__debug__/', include(debug_toolbar.urls)),)
 
 urlpatterns += urlpatterns_apirest
 urlpatterns += urlpatterns_agent_apirest

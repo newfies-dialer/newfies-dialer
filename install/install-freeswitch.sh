2177155fbbad45a564b674ac09b4ba9b18acf0a6
areski <areski@gmail.com>
2014-03-03 19:18:28 +0100

    support Debian Installation

--- install/install-freeswitch.sh
+++ install/install-freeswitch.sh
@@ -23,7 +23,6 @@ BRANCH='develop'
 
 FS_CONF_PATH=https://raw.github.com/Star2Billing/newfies-dialer/$BRANCH/install/freeswitch-conf
 FS_INIT_PATH=https://raw.github.com/Star2Billing/newfies-dialer/$BRANCH/install/freeswitch-init
-FS_INSTALLED_PATH=/usr/local/freeswitch
 FS_CONFIG_PATH=/etc/freeswitch
 FS_BASE_PATH=/usr/src/
 CURRENT_PATH=$PWD
@@ -37,28 +36,52 @@ if [ -f /etc/debian_version ] ; then
 elif [ -f /etc/redhat-release ] ; then
     DIST='CENTOS'
 else
-    echo "\n\nThis Installer should be run on a CentOS or Ubuntu."
+    echo "\n\nThis Installer should be run on a Debian 7.X or CentOS 6.X"
     exit 1
 fi
 
-
 clear
 echo ""
-echo "FreeSWITCH will be installed in $FS_INSTALLED_PATH"
+echo "FreeSWITCH will be installed!"
+echo ""
 echo "Press Enter to continue or CTRL-C to exit"
 echo ""
 read INPUT
 
-func_install_fs_source() {
-    #install fs from source
-    echo "Installing from source"
+
+# -- Functions --
+
+func_install_deps() {
+    echo "Setting up Prerequisites and Dependencies for FreeSWITCH"
+    case $DIST in
+        'DEBIAN')
+            apt-get -y update
+            apt-get -y install autoconf2.64 automake autotools-dev binutils bison build-essential cpp curl flex g++ gcc git-core libaudiofile-dev libc6-dev libdb-dev libexpat1 libexpat1-dev libgdbm-dev libgnutls-dev libmcrypt-dev libncurses5-dev libnewt-dev libpcre3 libpopt-dev libsctp-dev libsqlite3-dev libtiff4 libtiff4-dev libtool libx11-dev libxml2 libxml2-dev lksctp-tools lynx m4 make mcrypt ncftp nmap openssl sox sqlite3 ssl-cert ssl-cert unzip zip zlib1g-dev zlib1g-dev
+            apt-get -y install libssl-dev pkg-config
+            apt-get -y install libvorbis0a libogg0 libogg-dev libvorbis-dev
+            apt-get -y install flite flite1-dev
+            apt-get -y install unixodbc-dev odbc-postgresql
+            ;;
+        'CENTOS')
+            yum -y update
+            yum -y install autoconf automake bzip2 cpio curl curl-devel curl-devel expat-devel fileutils gcc-c++ gettext-devel gnutls-devel libjpeg-devel libogg-devel libtiff-devel libtool libvorbis-devel make ncurses-devel nmap openssl openssl-devel openssl-devel perl patch unixODBC unixODBC-devel unzip wget zip zlib zlib-devel
+            yum -y install git
+            yum -y install --enablerepo=epel flite flite-devel
+        ;;
+    esac
+}
+
+func_install_fs_sources() {
+    echo "Installing from sources"
+
+    #install Dependencies
+    func_install_deps
 
     #Add Freeswitch group and user
     grep -c "^freeswitch:" /etc/group &> /dev/null
     if [ $? = 1 ]; then
         /usr/sbin/groupadd -r -f freeswitch
     fi
-
     grep -c "^freeswitch:" /etc/passwd &> /dev/null
     if [ $? = 1 ]; then
         echo "adding user freeswitch..."
@@ -104,110 +127,115 @@ func_install_fs_source() {
     make && make install && make sounds-install && make moh-install
 
     # Remove temporary swap
-    #
     # swapoff /root/fakeswap
     # rm /root/fakeswap
-
 
     #Set permissions
     chown -R freeswitch:freeswitch /usr/local/freeswitch /etc/freeswitch
 }
 
+install_fs_deb_packages() {
+    #for 1.2 Stable Branch
+    echo 'deb http://files.freeswitch.org/repo/deb/debian/ wheezy main' >> /etc/apt/sources.list.d/freeswitch.list
 
-echo "Setting up Prerequisites and Dependencies for FreeSWITCH"
+    curl http://files.freeswitch.org/repo/deb/debian/freeswitch_archive_g0.pub | apt-key add -
+
+    #install Dependencies
+    func_install_deps
+
+    apt-get install freeswitch-meta-vanilla
+}
+
+func_configure_fs() {
+    echo "Enable FreeSWITCH modules"
+    cd $FS_CONFIG_PATH/autoload_configs/
+    [ -f modules.conf.xml ] && cp modules.conf.xml modules.conf.xml.bak
+    sed -i -r \
+    -e "s/<\!--\s?<load module=\"mod_lua\"\/>\s?-->/<load module=\"mod_lua\"\/>/g" \
+    -e "s/<\!--\s?<load module=\"mod_xml_curl\"\/>\s?-->/<load module=\"mod_xml_curl\"\/>/g" \
+    -e "s/<\!--\s?<load module=\"mod_xml_cdr\"\/>\s?-->/<load module=\"mod_xml_cdr\"\/>/g" \
+    -e "s/<\!--\s?<load module=\"mod_dingaling\"\/>\s?-->/<load module=\"mod_dingaling\"\/>/g" \
+    -e "s/<\!--\s?<load module=\"mod_shell_stream\"\/>\s?-->/<load module=\"mod_shell_stream\"\/>/g" \
+    -e "s/<\!-- \s?<load module=\"mod_shell_stream\"\/>\s? -->/<load module=\"mod_shell_stream\"\/>/g" \
+    -e "s/<\!--\s?<load module=\"mod_shout\"\/>\s?-->/<load module=\"mod_shout\"\/>/g" \
+    -e "s/<\!--\s?<load module=\"mod_tts_commandline\"\/>\s?-->/<load module=\"mod_tts_commandline\"\/>/g" \
+    -e "s/<\!--\s?<load module=\"mod_flite\"\/>\s?-->/<load module=\"mod_flite\"\/>/g" \
+    -e "s/<\!--\s?<load module=\"mod_say_ru\"\/>\s?-->/<load module=\"mod_say_ru\"\/>/g" \
+    -e "s/<\!--\s?<load module=\"mod_say_zh\"\/>\s?-->/<load module=\"mod_say_zh\"\/>/g" \
+    -e 's/mod_say_zh.*$/&\n    <load module="mod_say_de"\/>\n    <load module="mod_say_es"\/>\n    <load module="mod_say_fr"\/>\n    <load module="mod_say_it"\/>\n    <load module="mod_say_nl"\/>\n    <load module="mod_say_hu"\/>\n    <load module="mod_say_th"\/>/' \
+    modules.conf.xml
+
+    [ -f lua.conf.xml ] && mv lua.conf.xml lua.conf.xml.bak
+    wget --no-check-certificate $FS_CONF_PATH/lua.conf.xml -O lua.conf.xml
+
+    #Configure Dialplan
+    #cd $FS_CONFIG_PATH/conf/dialplan/
+    #Configure XML CDR
+    #cd $FS_CONFIG_PATH/conf/autoload_configs/
+
+    #this is commented as we don't use xml_cdr anymore
+    ## Place Newfies XML CDR conf in FreeSWITCH
+    #[ -f xml_cdr.conf.xml ] && mv xml_cdr.conf.xml xml_cdr.conf.xml.bak
+    #wget --no-check-certificate $FS_CONF_PATH/xml_cdr.conf.xml -O xml_cdr.conf.xml
+    #create dir to store send error of CDR
+    #mkdir /usr/local/freeswitch/log/err_xml_cdr/
+}
+
+func_create_alias_fs_cli() {
+    echo "Setup alias fs_cli"
+    alias fs_cli='/usr/local/freeswitch/bin/fs_cli'
+    chk=`grep "fs_cli" ~/.bashrc|wc -l`
+    if [ $chk -lt 1 ] ; then
+        echo "alias fs_cli='/usr/local/freeswitch/bin/fs_cli'" >> ~/.bashrc
+    fi
+}
+
+func_add_init_script() {
+    #Install init.d script
+    case $DIST in
+        'DEBIAN')
+            wget --no-check-certificate $FS_INIT_PATH/debian/freeswitch -O /etc/init.d/freeswitch
+            chmod 0755 /etc/init.d/freeswitch
+            cd /etc/init.d; update-rc.d freeswitch defaults 90
+        ;;
+        'CENTOS')
+            wget --no-check-certificate $FS_INIT_PATH/centos/freeswitch -O /etc/init.d/freeswitch
+            chmod 0755 /etc/init.d/freeswitch
+            chkconfig --add freeswitch
+            chkconfig --level 345 freeswitch on
+        ;;
+    esac
+}
+
+
 case $DIST in
     'DEBIAN')
-        apt-get -y update
-        apt-get -y install autoconf2.64 automake autotools-dev binutils bison build-essential cpp curl flex g++ gcc git-core libaudiofile-dev libc6-dev libdb-dev libexpat1 libexpat1-dev libgdbm-dev libgnutls-dev libmcrypt-dev libncurses5-dev libnewt-dev libpcre3 libpopt-dev libsctp-dev libsqlite3-dev libtiff4 libtiff4-dev libtool libx11-dev libxml2 libxml2-dev lksctp-tools lynx m4 make mcrypt ncftp nmap openssl sox sqlite3 ssl-cert ssl-cert unzip zip zlib1g-dev zlib1g-dev
-        apt-get -y install libssl-dev pkg-config
-        apt-get -y install libvorbis0a libogg0 libogg-dev libvorbis-dev
-        apt-get -y install flite flite1-dev
-        apt-get -y install unixodbc-dev odbc-postgresql
-        ;;
+        #Install FreeSWITCH from Debian packages
+        install_fs_deb_packages
+    ;;
     'CENTOS')
-        yum -y update
-        yum -y install autoconf automake bzip2 cpio curl curl-devel curl-devel expat-devel fileutils gcc-c++ gettext-devel gnutls-devel libjpeg-devel libogg-devel libtiff-devel libtool libvorbis-devel make ncurses-devel nmap openssl openssl-devel openssl-devel perl patch unixODBC unixODBC-devel unzip wget zip zlib zlib-devel
-        yum -y install git
-        yum -y install --enablerepo=epel flite flite-devel
+        #Install FreeSWITCH from sources
+        func_install_fs_sources
+
+        #Create alias fs_cli
+        #func_create_alias_fs_cli
+        #Install init.d script
+        #*Not need if installed from deb packages
+        #func_add_init_script
     ;;
 esac
 
-echo "Installing from source"
-#Install Freeswitch
-func_install_fs_source
+#Configure FreeSWITCH
+func_configure_fs
 
-# Enable FreeSWITCH modules
-cd $FS_CONFIG_PATH/autoload_configs/
-[ -f modules.conf.xml ] && cp modules.conf.xml modules.conf.xml.bak
-sed -i -r \
--e "s/<\!--\s?<load module=\"mod_lua\"\/>\s?-->/<load module=\"mod_lua\"\/>/g" \
--e "s/<\!--\s?<load module=\"mod_xml_curl\"\/>\s?-->/<load module=\"mod_xml_curl\"\/>/g" \
--e "s/<\!--\s?<load module=\"mod_xml_cdr\"\/>\s?-->/<load module=\"mod_xml_cdr\"\/>/g" \
--e "s/<\!--\s?<load module=\"mod_dingaling\"\/>\s?-->/<load module=\"mod_dingaling\"\/>/g" \
--e "s/<\!--\s?<load module=\"mod_shell_stream\"\/>\s?-->/<load module=\"mod_shell_stream\"\/>/g" \
--e "s/<\!-- \s?<load module=\"mod_shell_stream\"\/>\s? -->/<load module=\"mod_shell_stream\"\/>/g" \
--e "s/<\!--\s?<load module=\"mod_shout\"\/>\s?-->/<load module=\"mod_shout\"\/>/g" \
--e "s/<\!--\s?<load module=\"mod_tts_commandline\"\/>\s?-->/<load module=\"mod_tts_commandline\"\/>/g" \
--e "s/<\!--\s?<load module=\"mod_flite\"\/>\s?-->/<load module=\"mod_flite\"\/>/g" \
--e "s/<\!--\s?<load module=\"mod_say_ru\"\/>\s?-->/<load module=\"mod_say_ru\"\/>/g" \
--e "s/<\!--\s?<load module=\"mod_say_zh\"\/>\s?-->/<load module=\"mod_say_zh\"\/>/g" \
--e 's/mod_say_zh.*$/&\n    <load module="mod_say_de"\/>\n    <load module="mod_say_es"\/>\n    <load module="mod_say_fr"\/>\n    <load module="mod_say_it"\/>\n    <load module="mod_say_nl"\/>\n    <load module="mod_say_hu"\/>\n    <load module="mod_say_th"\/>/' \
-modules.conf.xml
 
-[ -f lua.conf.xml ] && mv lua.conf.xml lua.conf.xml.bak
-wget --no-check-certificate $FS_CONF_PATH/lua.conf.xml -O lua.conf.xml
+#Start FreeSWITCH
+/etc/init.d/freeswitch start
 
-#Configure Dialplan
-#cd $FS_CONFIG_PATH/conf/dialplan/
-
-#Configure XML CDR
-#cd $FS_INSTALLED_PATH/conf/autoload_configs/
-
-#this is commented as we don't use xml_cdr anymore
-## Place Newfies XML CDR conf in FreeSWITCH
-#[ -f xml_cdr.conf.xml ] && mv xml_cdr.conf.xml xml_cdr.conf.xml.bak
-#wget --no-check-certificate $FS_CONF_PATH/xml_cdr.conf.xml -O xml_cdr.conf.xml
-#create dir to store send error of CDR
-#mkdir /usr/local/freeswitch/log/err_xml_cdr/
-
-#Return to current path
 cd $CURRENT_PATH
 
-case $DIST in
-    'DEBIAN')
-        #Install init.d script
-        wget --no-check-certificate $FS_INIT_PATH/debian/freeswitch -O /etc/init.d/freeswitch
-        chmod 0755 /etc/init.d/freeswitch
-        cd /etc/init.d; update-rc.d freeswitch defaults 90
-    ;;
-    'CENTOS')
-        #Install init.d script
-        wget --no-check-certificate $FS_INIT_PATH/centos/freeswitch -O /etc/init.d/freeswitch
-        chmod 0755 /etc/init.d/freeswitch
-        chkconfig --add freeswitch
-        chkconfig --level 345 freeswitch on
-    ;;
-esac
-
-echo "Installing from source"
-#Add alias fs_cli
-alias fs_cli='/usr/local/freeswitch/bin/fs_cli'
-chk=`grep "fs_cli" ~/.bashrc|wc -l`
-if [ $chk -lt 1 ] ; then
-    echo "alias fs_cli='/usr/local/freeswitch/bin/fs_cli'" >> ~/.bashrc
-fi
-
-# Install Complete
 echo ""
-echo "********************************************************************"
-echo "Congratulations, FreeSWITCH is now installed at '$FS_INSTALLED_PATH'"
-echo "********************************************************************"
+echo "********************************************"
+echo "Congratulations, FreeSWITCH is now installed"
+echo "********************************************"
 echo
-echo "* To Start FreeSWITCH in foreground :"
-echo "    '$FS_INSTALLED_PATH/bin/freeswitch'"
-echo
-echo "* To Start FreeSWITCH in background :"
-echo "    '$FS_INSTALLED_PATH/bin/freeswitch -nc'"
-echo
-echo "********************************************************************"
-echo ""

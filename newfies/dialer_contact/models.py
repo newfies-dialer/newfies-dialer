e7be1f670d21cfe1109c6130ffbbae66a3e85abd
areski <areski@gmail.com>
2014-03-18 01:40:46 +0100

    fix unicode error on SMS campaign

--- newfies/dialer_contact/models.py
+++ newfies/dialer_contact/models.py
@@ -143,7 +143,7 @@ class Contact(Model):
 
         as well as, get additional_vars, and replace json tags
         """
-        text = str(text).lower()
+        #text = text.encode('utf-8')
         taglist = {
             'last_name': self.last_name,
             'first_name': self.first_name,

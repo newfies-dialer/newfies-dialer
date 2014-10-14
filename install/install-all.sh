617a318dab1d8c8787581dc2ef8ab024f52eefb8
areski <areski@gmail.com>
2014-10-15 00:25:23 +0200

    change script notification to run on Debian

--- install/install-all.sh
+++ install/install-all.sh
@@ -24,30 +24,31 @@
 #Set branch to install develop / master
 BRANCH='master'
 
+SCRIPT_NOTICE="This script is only intended to run on Debian 7.X"
+
 # Identify Linux Distribution type
 func_identify_os() {
     if [ -f /etc/debian_version ] ; then
         DIST='DEBIAN'
         apt-get -y install lsb-release
         if [ "$(lsb_release -cs)" != "wheezy" ]; then
-            echo "This script is only intended to run on Debian 7.X or CentOS 6.X"
+            echo $SCRIPT_NOTICE
             exit 255
         fi
     elif [ -f /etc/redhat-release ] ; then
         DIST='CENTOS'
         if [ "$(awk '{print $3}' /etc/redhat-release)" != "6.2" ] && [ "$(awk '{print $3}' /etc/redhat-release)" != "6.3" ] && [ "$(awk '{print $3}' /etc/redhat-release)" != "6.4" ] && [ "$(awk '{print $3}' /etc/redhat-release)" != "6.5" ]; then
-            echo "This script is only intended to run on Debian 7.X or CentOS 6.X"
+            echo $SCRIPT_NOTICE
             exit 255
         fi
     else
-        echo "This script is only intended to run on Debian 7.X or CentOS 6.X"
+        echo $SCRIPT_NOTICE
         exit 1
     fi
 }
 
 #install the epel repository.
 func_install_epel_repo() {
-
 	if [ ! -f /etc/yum.repos.d/epel.repo ];
 		then
 			echo '

e874d0028979f36a6e859cceb1922c8439135e81
areski <areski@gmail.com>
2014-11-22 00:46:44 +0100

    add migration for changes on appointment CalendarSetting

---
+++ newfies/appointment/migrations/0002_auto_20141122_0045.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('appointment', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='calendarsetting',
+            name='callerid',
+            field=models.CharField(help_text='outbound Caller ID', max_length=80, null=True, verbose_name='Caller ID Number', blank=True),
+        ),
+        migrations.AlterField(
+            model_name='calendarsetting',
+            name='sms_gateway',
+            field=models.ForeignKey(related_name=b'sms_gateway', blank=True, to='sms.Gateway', help_text='select SMS gateway', null=True, verbose_name='SMS gateway'),
+        ),
+    ]

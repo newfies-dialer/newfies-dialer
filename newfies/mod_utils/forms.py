9ef2d84fd9cf430be1fb1e434e4097c1a187f644
areski <areski@gmail.com>
2014-11-28 16:43:52 +0100

    fix some lazy translation

--- newfies/mod_utils/forms.py
+++ newfies/mod_utils/forms.py
@@ -14,7 +14,7 @@
 from django import forms
 from django.forms import ModelForm
 from django.utils.safestring import mark_safe
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import ugettext as _
 from mod_utils.helper import Export_choice
 
 from crispy_forms.layout import HTML
--- newfies/mod_utils/function_def.py
+++ newfies/mod_utils/function_def.py
@@ -12,7 +12,7 @@
 # Arezqui Belaid <info@star2billing.com>
 #
 
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import ugettext as _
 
 
 def tpl_control_icon(icon):

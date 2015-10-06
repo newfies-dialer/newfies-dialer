cf2f0c8084a4e9144f89c420f5668f8681d2f876
joshpatten <joshpatten@gmail.com>
2015-10-06 10:04:29 -0500

    MSSPeak Enhancement
    Commit to add MSSpeak to Newfies-Dialer

--- lua/libs/settings.lua
+++ lua/libs/settings.lua
@@ -9,19 +9,36 @@ DBPASS = 'password'
 DBPORT = 5432
 
 --
--- Select the TTS engine, value : flite, acapela
+-- Select the TTS engine, value : flite, acapela, msspeak
 --
 TTS_ENGINE = 'flite'
 
 --
--- Acapela TTS Settings
+-- Acapela and MSSpeak TTS Settings
+-- MSSpeak only needs APPLICATION_LOGIN(client_id)
+-- and APPLICATION_PASSWORD(client_secret).
+-- ACCOUNT_LOGIN is ignored for MSSpeak
 --
 ACCOUNT_LOGIN = 'EVAL_VAAS'
 APPLICATION_LOGIN = 'EVAL_YYYYYYY'
 APPLICATION_PASSWORD = 'XXXXXXXX'
 
+--
+-- For MSSpeak comment out Acapela SERVICE_URL
+-- and uncomment other SERVICE_URL parameter
+--
+-- SERVICE_URL = 'http://api.microsofttranslator.com/V2/Http.svc/Speak'
 SERVICE_URL = 'http://vaas.acapela-group.com/Services/Synthesizer'
+
+--
+-- Acapela Specific Settings
+--
 QUALITY = '22k'  -- 22k, 8k, 8ka, 8kmu
 ACAPELA_GENDER = 'W'
 ACAPELA_INTONATION = 'NORMAL'
 ACAPELA_LANG = 'EN'
+
+--
+-- Microsoft Speak Specific Settings
+--
+MSSPEAK_LANGUAGE = 'EN'

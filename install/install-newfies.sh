55f540f361afee90d285bda0a7c03668392f65ee
Asraful Kabir <afkmamunbd@engineer.com>
2019-06-30 06:20:09 +0600

    Update install-newfies.sh

--- install/install-newfies.sh
+++ install/install-newfies.sh
@@ -1,21 +1,13 @@
 #!/bin/bash
-#
 # Newfies-Dialer License
 # http://www.newfies-dialer.org
-#
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this file,
 # You can obtain one at http://mozilla.org/MPL/2.0/.
-#
 # Copyright (C) 2011-2014 Star2Billing S.L.
-#
 # The Initial Developer of the Original Code is
 # Arezqui Belaid <info@star2billing.com>
-#
-
-#
 # To download and run the script on your server :
-#
 # >> Install with Master script :
 # cd /usr/src/ ; rm install-newfies.sh ; wget --no-check-certificate https://raw.github.com/Star2Billing/newfies-dialer/master/install/install-newfies.sh ; chmod +x install-newfies.sh ; ./install-newfies.sh
 #
